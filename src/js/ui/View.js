a7d887d5312edd55e921b324bc2f73ef33562b75
Marcus Krejpowicz <mkre@m186.fritz.box>
2012-02-09 23:26:45 +0100

    button template example

--- src/js/ui/View.js
+++ src/js/ui/View.js
@@ -1,9 +1,21 @@
 rAppid.defineClass("js.ui.View",
-    ["underscore", "js.core.UIComponent"], function (_, UIComponent) {
+    ["underscore", "js.core.UIComponent", "js.core.Template"], function (_, UIComponent, Template) {
         return UIComponent.inherit({
             _defaults: {
                 tagName: "div",
                 items: []
+            },
+            render: function(){
+                if(this.isRendered()){
+                   return this.$el;
+                }
+                console.log(this);
+                var template = this.getTemplate('button-tpl');
+                if(template){
+                    console.log(template);
+                }else{
+                    return this.callBase();
+                }
             },
             _renderAttribute:function (key, attribute) {
                 // generic call of render functions
