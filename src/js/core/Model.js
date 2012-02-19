64094ef635cf0522edc11d0241d69e5a37d06728
it-ony <github@it-ony.de>
2012-02-19 11:48:02 +0100

    abstract model

--- src/js/core/Model.js
+++ src/js/core/Model.js
@@ -1,7 +1,19 @@
-rAppid.defineClass("js.core.Model", ["js.core.EventDispatcher"], function(EventDispatcher) {
-    return EventDispatcher.inherit({
-        ctor: function() {
-            this.callBase();
+rAppid.defineClass("js.core.Model", ["js.core.Bindable"], function(Bindable) {
+    return Bindable.inherit({
+        ctor: function(attributes) {
+            this.callBase(attributes);
+        },
+        create: function(options, callback) {
+
+        },
+        save: function(options, callback) {
+
+        },
+        fetch: function(options, callback) {
+
+        },
+        "delete": function(options, callback) {
+
         }
     })
 });
