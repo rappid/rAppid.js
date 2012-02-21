56c5fc555da218093bd76028ab292570a6cbfe95
Marcus Krejpowicz <mkre@m186.itdev.hq.sprd.net>
2012-02-21 16:02:55 +0100

    - removed some basic methods

--- src/js/core/UIComponent.js
+++ src/js/core/UIComponent.js
@@ -9,17 +9,6 @@ rAppid.defineClass("js.core.UIComponent",
                     this._renderAttributes(attributes);
                 }
             },
-            _renderAttribute:function (key, attribute) {
-                var prev = this.$previousAttributes[key];
-
-                // generic call of render functions
-                key = key[0].toUpperCase() + key.substr(1);
-                var methodName = "_render" + key;
-                var method = this[methodName];
-                if (_.isFunction(method)) {
-                    method.call(this, attribute, prev);
-                }
-            },
             _initializationComplete: function(){
                 this.callBase();
             }
