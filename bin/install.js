07b056583647405d6deea1e2696a1b097dc86d56
Marcus Krejpowicz <krebbl@gmail.com>
2012-04-04 21:46:12 +0200

    try for install.js

--- bin/install.js
+++ bin/install.js
@@ -4,8 +4,9 @@ install.usage = "rappidjs install <pkg>"
     + "\nrappidjs install <pkg> <version>"
     + "\nInstalls rAppidjs dependencies from ./package.json.";
 
-var npm = require("npm.js"),
-    fs = require("fs"),
+var sys = require('sys')
+var exec = require('child_process').exec;
+var fs = require("fs"),
     path = require("path"),
     readJson = require("npm/utils/read-json.js"),
     flow = require("flow.js").flow;
@@ -13,51 +14,49 @@ var npm = require("npm.js"),
 
 var dir;
 
-function install(args, callback){
-    if(args.length  === 0){
+function install(args, callback) {
+    if (args.length === 0) {
         callback("No args defined");
     }
     var packageName = args.shift();
     var what = packageName;
-    if(args.length > 0){
-        what += "@"+args.shift();
+    if (args.length > 0) {
+        what += "@" + args.shift();
     }
 
-    // try to install package
-    npm.commands.install(what, function(err, installed){
-        if(!err){
-            if(installed){
+    // executes `pwd`
+    exec("nmp install" + what, function (error, stdout, stderr) {
+        sys.print('stdout: ' + stdout);
+        sys.print('stderr: ' + stderr);
+        if (!err) {
 
-                dir = dir || process.cwd();
+            dir = dir || process.cwd();
 
-                var publicDir = path.join(dir,"Public");
-                var where = path.resolve(npm.dir, "..");
-                var packageDir = path.join(dir,"node_modules",packageName);
-                readJson(path.join(packageDir,"package.json"),function(er, data){
-                    if (er) data = null;
+            var publicDir = path.join(dir, "Public");
+            var packageDir = path.join(__dirname,"..","node_modules", packageName);
+            readJson(path.join(packageDir, "package.json"), function (er, data) {
+                if (er) data = null;
 
-                    fs.symlinkSync(path.join(publicDir, data.lib), path.join(packageDir, data.lib));
+                fs.symlinkSync(path.join(publicDir, data.lib), path.join(packageDir, data.lib));
 
-                    var dependencies = data.rAppidDependencies;
-                    var f = flow();
-                    function doInstall(dName,dVersion, cb){
-                        install([dName, dVersion].join("@"), cb);
+                var dependencies = data.rAppidDependencies;
+                var f = flow();
+
+                function doInstall(dName, dVersion, cb) {
+                    install([dName, dVersion].join("@"), cb);
+                }
+
+                for (var key in dependencies) {
+                    if (dependencies.hasOwnProperty(key)) {
+                        f.seq(function (cb) {
+                            doInstall(key, dependencies[key], cb);
+                        });
                     }
-                    for(var key in dependencies){
-                        if(dependencies.hasOwnProperty(key)){
-                            f.seq(function (cb) {
-                                doInstall(key,dependencies[key],cb);
-                            });
-                        }
-                    }
-                    f.exec(callback);
-                });
+                }
+                f.exec(callback);
+            });
 
-
-            }else{
-                callback("Installation failed");
-            }
-        }else{
+        } else {
             callback("Error while installing " + packageName);
         }
     });
