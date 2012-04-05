931c08a6ab6140f60ec29a6f9735ec41edfab109
Marcus Krejpowicz <krebbl@gmail.com>
2012-04-05 17:04:16 +0200

    added change dir for install script

--- bin/install.js
+++ bin/install.js
@@ -37,14 +37,18 @@ function install(args, callback) {
 
     var originalWD = process.cwd();
     // change dir
-    // process.chdir(dir);
+    process.chdir(dir);
 
     child = child_process.exec(["npm","install",what,"-d"].join(" "), function (err, stdout, stderr) {
         sys.print('stdout: ' + stdout);
         sys.print('stderr: ' + stderr);
 
         if(!err){
-            linkPackage(dir, packageName, callback);
+            linkPackage(dir, packageName, function(err){
+                process.chdir(originalWD);
+
+                callback(err);
+            });
         }else{
             callback(err);
         }
