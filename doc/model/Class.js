70fa049d86fea44015347beb06bc991fca8f35c4
it-ony <github@it-ony.de>
2012-04-05 09:46:31 +0200

    removed unfinished doc application

--- doc/model/Class.js
+++
@@ -1,8 +0,0 @@
-var requirejs = (typeof requirejs === "undefined" ? require("requirejs") : requirejs);
-
-requirejs(["rAppid"], function (rAppid) {
-    rAppid.defineClass("doc.model.Class", ["js.data.Model"], function (Model) {
-        return Model.inherit({
-        });
-    });
-});
