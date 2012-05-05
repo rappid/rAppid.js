7f96913a27a3d79757f34aab62a1d8201ed4d744
Tony Findeisen <github@it-ony.de>
2012-05-05 22:02:23 +0200

    filtered name attribute

--- js/ui/ContentPlaceHolder.js
+++ js/ui/ContentPlaceHolder.js
@@ -1,5 +1,8 @@
 define(["js/ui/View"], function (View) {
     return View.inherit(({
+
+        $classAttributes: ["name"],
+
         _renderContent: function (content) {
             this._clearRenderedChildren();
             if (content) {
