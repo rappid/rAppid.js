70b431841d17c9ce14d251a214b3f7846a39ba1c
Tony Findeisen <github@it-ony.de>
2012-05-07 22:09:12 +0200

    added subclassed a tag

---
+++ js/html/a.js
@@ -0,0 +1,37 @@
+define(['js/html/DomElement'], function (DomElement) {
+
+    var externalLink = /^(([^:]+:\/\/)|(javascript:))/i,
+        startsWithHash = /^#/,
+        hashBankUrl = /^#?(.*)$/;
+
+    return DomElement.inherit("js.html.a", {
+        defaults: {
+            tagName: 'a',
+            target: null,
+            href: null
+        },
+        _renderHref: function (href) {
+            href = href || "javascript:void(0);";
+
+            if (!(this.$.target === "external" || externalLink.test(href))) {
+                // ajax link
+                if (!startsWithHash.test(href)) {
+                    href = "#" + href;
+                }
+
+                if (!this.runsInBrowser()) {
+                    // node rendering -> hash bang url
+                    href = href.replace(hashBankUrl, "#!$1");
+                }
+            }
+
+            this.$el.setAttribute("href", href);
+        },
+
+        _renderTarget: function(target) {
+            if (target !== "external") {
+                this.$el.setAttribute("target", target);
+            }
+        }
+    });
+});
