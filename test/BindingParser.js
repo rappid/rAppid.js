b63d43e2ca1ad70f1d3d6f7cb19af4ff08893a78
Marcus Krejpowicz <krebbl@gmail.com>
2012-05-12 15:13:51 +0200

    added tests for Binding Parser

---
+++ test/BindingParser.js
@@ -0,0 +1,299 @@
+var should = require('chai').should();
+var requirejs = require('./TestRunner').require;
+var flow = require('flow.js').flow;
+
+var Parser = requirejs("js/core/BindingParser");
+
+
+describe('js.core.BindingParser', function () {
+
+    describe("#parse varName", function () {
+
+        it('should parse valid var names', function(done){
+            var validNames = ["asd", "a123", "$asd", "_asd", "a_", "Abc"];
+            var parsed, RULE = "varName";
+            flow().seqEach(validNames,
+                function (name, cb) {
+                    try {
+                        parsed = Parser.parse(name, RULE);
+                        cb();
+                    } catch(e) {
+                        cb(e);
+                    }
+                }).exec(done);
+        });
+
+        it('should not parse an invalid var name', function (done) {
+            // todo: complete the list
+            var invalidNames = ["123asd", "-asd", "+asd", "/asd", "asd()", ".asdasd"];
+            var parsed, RULE = "varName";
+            flow().seqEach(invalidNames,
+                function (name, cb) {
+                    try {
+                        parsed = Parser.parse(name, RULE);
+                    } catch(e) {
+                        cb();
+                    }
+                }).exec(done);
+        });
+    });
+
+    describe('#parse var', function () {
+
+        var RULE = "var", parsed;
+
+        it('should return a object with name and type', function () {
+            var varName = "a";
+
+            parsed = Parser.parse(varName, RULE);
+            parsed.type.should.equal('var');
+            parsed.name.should.equal(varName);
+        });
+
+    });
+
+    describe('#parse fnc', function () {
+        var RULE = "fnc", parsed;
+
+        it('should return a object with name, type and parameter', function () {
+            var fncName = "abc";
+
+            parsed = Parser.parse(fncName + "()", RULE);
+            parsed.type.should.equal('fnc');
+            parsed.name.should.equal(fncName);
+            parsed.parameter.length.should.equal(0);
+        });
+
+        it('should return a object with name, type and parameter', function () {
+            var fncName = "abc";
+
+            parsed = Parser.parse(fncName + "()", RULE);
+            parsed.type.should.equal('fnc');
+            parsed.name.should.equal(fncName);
+            parsed.parameter.length.should.equal(0);
+        });
+
+        it('should return a object with an array of parameters', function () {
+            var fncName = "abc";
+
+            parsed = Parser.parse(fncName + "('hello',123,{binding})", RULE);
+            parsed.type.should.equal('fnc');
+            parsed.name.should.equal(fncName);
+            parsed.parameter.length.should.equal(3);
+        });
+
+    });
+
+    describe('#parse parameter', function () {
+        var parsed, RULE = "parameter";
+        it('should parse a single escaped string', function () {
+            var string = "myString";
+            var def = "'" + string + "'";
+            parsed = Parser.parse(def, RULE);
+
+            parsed.should.equal(string);
+        });
+
+        it('should parse a double escaped string', function () {
+            var string = "myString";
+            var def = "\"" + string + "\"";
+            parsed = Parser.parse(def, RULE);
+
+            parsed.should.equal(string);
+        });
+
+        it('should parse a integer', function () {
+            var integer = 123123;
+            var def = "" + integer;
+            parsed = Parser.parse(def, RULE);
+
+            parsed.should.equal(integer);
+        });
+
+        it('should parse a float', function () {
+            var flo = 12.23;
+            var def = "" + flo;
+            parsed = Parser.parse(def, RULE);
+
+            parsed.should.equal(flo);
+        });
+
+        it('should parse a boolean', function () {
+            Parser.parse("true", RULE).should.equal(true);
+            Parser.parse("false", RULE).should.equal(false);
+        });
+
+        it('should parse a binding', function () {
+            should.exist(Parser.parse("{abc}", RULE));
+        });
+
+        it('should parse static binding', function () {
+            should.exist(Parser.parse("${abc}", RULE));
+        });
+
+        it('should not parse two way binding', function (done) {
+            try {
+                Parser.parse("{{abc}}", RULE);
+                done("TwoWay binding should not be parsed by path parser");
+            } catch(e) {
+                done();
+            }
+        });
+
+    });
+
+    describe('#parse parameterArray', function () {
+        var RULE = "parameterArray";
+
+        it("should parse a , separated parameter list", function(){
+            var string = "'abc',123213,{binding}";
+            Parser.parse(string,RULE).length.should.equal(3);
+        });
+
+        it("should not parse a , separated parameter list with spaces", function (done) {
+            var string = "'abc'   ,   123213  , {binding}";
+
+            try {
+                Parser.parse(string, RULE);
+                done("should not parse a , separated list with spaces")
+            } catch(e) {
+                done();
+            }
+        });
+
+    });
+
+
+        describe('#parse path', function () {
+        var RULE = "path";
+
+        it('should parse a path of varNames', function () {
+            var path = ["a", "b"];
+            var parsed = Parser.parse(path.join("."), RULE);
+
+            parsed.length.should.equal(path.length);
+            parsed[0].type.should.equal("var");
+            parsed[0].name.should.equal("a");
+
+            parsed[1].type.should.equal("var");
+            parsed[1].name.should.equal("b");
+
+        });
+
+        it('should parse a path of mixed values', function () {
+            var path = ["a", "b()"];
+            var parsed = Parser.parse(path.join("."), RULE);
+
+            parsed.length.should.equal(path.length);
+            parsed[0].type.should.equal("var");
+            parsed[0].name.should.equal("a");
+
+            parsed[1].type.should.equal("fnc");
+            parsed[1].name.should.equal("b");
+        });
+    });
+
+    describe('#parse binding', function () {
+        var RULE = "binding", parsed, NORMAL = "normal";
+
+        it('should parse a binding like {asd}', function(){
+            parsed = Parser.parse('{asd}',RULE);
+            parsed.type.should.equal(NORMAL);
+            parsed.path.length.should.equal(1);
+        });
+
+        it('should not parse a binding like {{asd}', function () {
+            parsed = Parser.parse('{asd}', RULE);
+            parsed.type.should.equal(NORMAL);
+            parsed.path.length.should.equal(1);
+        });
+
+    });
+
+    describe('#parse two way binding', function () {
+        var RULE = "twoWayBinding", parsed, TWOWAY = "twoWay";
+
+        it('should parse a binding like {asd}', function () {
+            parsed = Parser.parse('{{asd}}', RULE);
+            parsed.type.should.equal(TWOWAY);
+            parsed.path.length.should.equal(1);
+        });
+
+    });
+
+    describe('#parse static binding', function(){
+        var RULE = "staticBinding", parsed;
+
+        it('should parse a binding like ${asd}', function() {
+            parsed = Parser.parse('${asd}', RULE);
+            parsed.type.should.equal('static');
+            parsed.path.length.should.equal(1);
+        });
+
+    });
+
+    describe('#parse text', function() {
+
+        var RULE = "text", parsed;
+
+
+        it('should return an array of char if only text', function () {
+            var text = "ABC";
+            Parser.parse(text, RULE).join("").should.equal(text);
+        });
+
+        it('should return an array of normal binding definitions', function () {
+            var text = "{what}{isGoing}{On}";
+            parsed = Parser.parse(text, RULE);
+
+            for (var i = 0; i < parsed.length; i++) {
+                parsed[i].type.should.equal('normal');
+            }
+        });
+
+        it('should return an array of static binding definitions', function () {
+            var text = "${what}${isGoing}${On}";
+            parsed = Parser.parse(text, RULE);
+
+            for (var i = 0; i < parsed.length; i++) {
+                parsed[i].type.should.equal('static');
+            }
+        });
+
+        it('should return an array of mixed binding definitions', function () {
+            var text = "${what}{isGoing}${On}";
+            parsed = Parser.parse(text, RULE);
+
+            parsed[0].type.should.equal('static');
+            parsed[1].type.should.equal('normal');
+            parsed[2].type.should.equal('static');
+        });
+
+        it('should return an array of chars and binding definitions', function () {
+            var text = "${what}a{isGoing}b${On}";
+            parsed = Parser.parse(text, RULE);
+
+            parsed[0].type.should.equal('static');
+            parsed[1].should.equal('a');
+            parsed[2].type.should.equal('normal');
+            parsed[3].should.equal('b');
+            parsed[4].type.should.equal('static');
+        });
+
+        it('should return a twoWayBinding definition for {{asdasd}}', function () {
+            var text = "{{asdasd}}";
+            Parser.parse(text, RULE)[0].type.should.equal("twoWay");
+        });
+
+        it('should not parse a text with mixed twoWay and normal bindings', function (done) {
+            var text = "{{what}} {isGoing}{On}";
+            try {
+                Parser.parse(text, RULE);
+                done('should not parse a text with mixed twoWay and normal bindings');
+            } catch(e) {
+                done();
+            }
+        });
+    })
+
+});
