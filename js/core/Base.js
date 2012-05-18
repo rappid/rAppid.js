38bc27d6b5122e5f305e42f1f29bb91caf55ab50
Tony Findeisen <github@it-ony.de>
2012-05-18 17:03:56 +0200

    added log to js.core.bar

--- js/core/Base.js
+++ js/core/Base.js
@@ -1,11 +1,43 @@
 define(["inherit"], function(inherit){
 
-    return inherit.Base.inherit("js.core.Base",{
+    var Base = inherit.Base.inherit("js.core.Base",{
         /** @lends Base **/
         ctor: function () {
         },
+
         runsInBrowser: function () {
             return typeof window !== "undefined";
+        },
+
+        /***
+         *
+         * @param {String|Array} message
+         * @param {String} [level="info"]
+         */
+        log: function(message, level) {
+            level = level || Base.LOGLEVEL.INFO;
+
+            if (message instanceof Error) {
+                message = message.toString();
+            }
+
+            if (Base.logger.length) {
+                for (var i = 0; i < Base.logger.length; i++) {
+                    Base.logger[i].log(message, level);
+                }
+            } else if (typeof console !== "undefined") {
+                (console[level] || console.log)(message);
+            }
         }
     });
+
+    Base.logger = [];
+    Base.LOGLEVEL = {
+        DEBUG: 'debug',
+        INFO: 'info',
+        WARN: 'warn',
+        ERROR: 'error'
+    };
+
+    return Base;
 });
