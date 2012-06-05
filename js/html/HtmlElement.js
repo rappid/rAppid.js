96a5ae14c6ca9057148c0097080bbe4942d6b884
Tony Findeisen <github@it-ony.de>
2012-06-05 22:14:49 +0200

    moved html methods from DomElement int HtmlElement

---
+++ js/html/HtmlElement.js
@@ -0,0 +1,65 @@
+define(['js/html/DomElement'], function(DomElement) {
+
+    return DomElement.inherit("js/html/HtmlElement", {
+
+        defaults: {
+            selected: undefined,
+            selectable: undefined
+        },
+
+        _renderVisible: function (visible) {
+            if (visible === true) {
+                this.removeClass('hide');
+            } else if (visible === false) {
+                this.addClass('hide');
+            }
+        },
+
+        _renderHidden: function (hidden) {
+            if (typeof(hidden) !== "undefined") {
+                this.set({visible: !hidden});
+            }
+        },
+
+        _renderSelected: function (selected) {
+            if (selected === true) {
+                this.addClass('active');
+            } else if (selected === false) {
+                this.removeClass('active');
+            }
+        },
+
+        _renderSelectable: function (selectable) {
+            if (selectable === true) {
+                if (!this._onSelect) {
+                    var self = this;
+                    this._onSelect = function () {
+                        self.set({selected: !self.$.selected});
+                    };
+                }
+                this.addEventListener('click', this._onSelect);
+            } else {
+                if (this._onSelect) {
+                    this.removeEvent('click', this._onSelect);
+                }
+            }
+        },
+        _renderWidth: function (width) {
+            if (width) {
+                if (typeof(width) !== "string") {
+                    width += "px";
+                }
+                this.$el.style.width = width;
+            }
+        },
+        _renderHeight: function (height) {
+            if (height) {
+                if (typeof(height) !== "string") {
+                    height += "px";
+                }
+                this.$el.style.height = height;
+            }
+        }
+
+    });
+});
