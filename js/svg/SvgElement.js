a54050d662f8b3ed008a0bc79f64e3bf6540226d
Tony Findeisen <github@it-ony.de>
2012-06-06 20:07:26 +0200

    added SvgElement with namespaces

--- js/svg/SvgElement.js
+++ js/svg/SvgElement.js
@@ -1,7 +1,22 @@
 define(['js/html/DomElement'], function(DomElement) {
-    return DomElement.inherit("js/svg/SvgElement", {
-        _renderAttributes: function() {
+
+    var SvgElement = DomElement.inherit("js/svg/SvgElement", {
+
+        ctor: function() {
+            // default namespace
+            this.$namespace = SvgElement.SVG_NAMESPACE;
+
             this.callBase();
+        },
+
+        // render each href Element in xlink namespace
+        _renderHref: function(href, oldHref) {
+            this._setAttribute("href", href, SvgElement.XLINK_NAMESPACE);
         }
     });
+
+    SvgElement.XLINK_NAMESPACE = "http://www.w3.org/1999/xlink";
+    SvgElement.SVG_NAMESPACE = "http://www.w3.org/2000/svg";
+
+    return SvgElement;
 });
