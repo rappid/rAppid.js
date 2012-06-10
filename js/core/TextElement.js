47ce0833bb97cb2ffe3c3173593ed092e878c8f3
Marcus Krejpowicz <krebbl@gmail.com>
2012-06-10 19:05:48 +0200

    fixed creating text node

--- js/core/TextElement.js
+++ js/core/TextElement.js
@@ -16,7 +16,7 @@ define(
                     this._initialize(this.$creationPolicy);
                 }
 
-                this.$el = this.$systemManager.$document.createTextNode();
+                this.$el = this.$systemManager.$document.createTextNode("");
                 this._renderTextContent(this.$.textContent);
 
 
