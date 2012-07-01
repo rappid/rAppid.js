5608c602771599c75cf4c3709eecf3dbe1650d8d
Tony Findeisen <github@it-ony.de>
2012-07-01 15:32:31 +0200

    Revert "added text plugin and changed json plugin ..."
    This reverts commit cb21359686193d38fd3eba93b0ba7fab7c9ed3f5.

--- js/plugins/json.js
+++ js/plugins/json.js
@@ -1,56 +1,107 @@
-/** @license
- * RequireJS plugin for loading JSON files
- * - depends on Text plugin and it was HEAVILY "inspired" by it as well.
- * Author: Miller Medeiros
- * Version: 0.2.1 (2012/04/17)
- * Released under the MIT license
- */
-define(['text'], function (text) {
-    var CACHE_BUST_QUERY_PARAM = 'bust',
-        CACHE_BUST_FLAG = '!bust',
-        jsonParse = (typeof JSON !== 'undefined' && typeof JSON.parse === 'function') ? JSON.parse : function (val) {
-            return eval('(' + val + ')'); //quick and dirty
+define(['JSON'], function (JSON) {
+    var fs, createXhr,
+        progIds = ['Msxml2.XMLHTTP', 'Microsoft.XMLHTTP', 'Msxml2.XMLHTTP.4.0'],
+        fetchJSON = function (url, callback) {
+            throw new Error('Environment unsupported.');
         },
-        buildMap = {};
+        buildMap = {},
+        rSuffix = /^(.+)\.json$/i;
 
-    function cacheBust(url) {
-        url = url.replace(CACHE_BUST_FLAG, '');
-        url += (url.indexOf('?') < 0) ? '?' : '&';
-        return url + CACHE_BUST_QUERY_PARAM + '=' + Math.round(2147483647 * Math.random());
+
+    if ((typeof window !== "undefined" && window.navigator && window.document) || typeof importScripts !== "undefined") {
+        // Browser action
+        createXhr = function () {
+            //Would love to dump the ActiveX crap in here. Need IE 6 to die first.
+            var xhr, i, progId;
+            if (typeof XMLHttpRequest !== "undefined") {
+                return new XMLHttpRequest();
+            } else {
+                for (i = 0; i < 3; i++) {
+                    progId = progIds[i];
+                    try {
+                        xhr = new ActiveXObject(progId);
+                    } catch(e) {
+                    }
+
+                    if (xhr) {
+                        progIds = [progId];  // so faster next time
+                        break;
+                    }
+                }
+            }
+
+            if (!xhr) {
+                throw new Error("getXhr(): XMLHttpRequest not available");
+            }
+
+            return xhr;
+        };
+
+        fetchJSON = function (url, callback) {
+            var xhr;
+
+            try {
+                xhr = createXhr();
+                xhr.open('GET', url, true);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === 4) {
+                        if ((xhr.status === 200 || xhr.status === 304) && xhr.responseText) {
+                            callback(null, xhr.responseText);
+                        } else {
+                            callback("no responseXML found");
+                        }
+                    }
+                };
+                xhr.send(null);
+            } catch(e) {
+                callback(e);
+            }
+        };
+        // end browser.js adapters
+    } else if (typeof process !== "undefined" && process.versions && !!process.versions.node) {
+        //Using special require.nodeRequire, something added by r.js.
+        fs = require.nodeRequire('fs');
+
+        fetchJSON = function (path, callback) {
+            try {
+                var content = fs.readFileSync(path, 'utf8');
+                callback(null, content);
+            } catch(e) {
+                callback(e);
+            }
+        };
     }
 
-    //API
+
     return {
 
-        load: function (name, req, onLoad, config) {
-            if (config.isBuild && (config.inlineJSON === false || name.indexOf(CACHE_BUST_QUERY_PARAM + '=') !== -1)) {
-                //avoid inlining cache busted JSON or if inlineJSON:false
-                onLoad(null);
-            } else {
-                text.get(req.toUrl(name), function (data) {
-                    if (config.isBuild) {
-                        buildMap[name] = data;
-                        onLoad(data);
-                    } else {
-                        onLoad(jsonParse(data));
-                    }
-                });
+        write: function (pluginName, name, write) {
+
+            if (name in buildMap) {
+                var text = buildMap[name];
+                write.asModule(pluginName + "!" + name, text);
             }
         },
 
-        normalize: function (name, normalize) {
-            //used normalize to avoid caching references to a "cache busted" request
-            return (name.indexOf(CACHE_BUST_FLAG) === -1) ? name : cacheBust(name);
-        },
+        version: '0.1.0',
 
-        //write method based on RequireJS official text plugin by James Burke
-        //https://github.com/jrburke/requirejs/blob/master/text.js
-        write: function (pluginName, moduleName, write) {
-            if (moduleName in buildMap) {
-                var content = buildMap[moduleName];
-                write('define("' + pluginName + '!' + moduleName + '", function(){ return ' + content + ';});\n');
-            }
+        load: function (name, parentRequire, load, config) {
+
+
+            name = name.replace(rSuffix,"$1");
+            name = name + ".json";
+
+            var url = parentRequire.toUrl(name );
+
+            fetchJSON(url, function (err, raw) {
+                if (!err) {
+                    load(JSON.parse(raw));
+                } else {
+                    load.error(new Error("Json for " + url + " not found"));
+                }
+            });
         }
-
-    };
+    }
 });
+
+
