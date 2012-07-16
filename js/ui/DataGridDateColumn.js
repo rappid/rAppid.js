9f437c91d325a57c570ecf808c32cf2793e16161
Marcus Krejpowicz <krebbl@gmail.com>
2012-07-16 09:24:13 +0200

    added date grid column

---
+++ js/ui/DataGridDateColumn.js
@@ -0,0 +1,21 @@
+define(['xaml!js/ui/DataGridColumn','js/lib/moment'], function (Column, moment) {
+
+    return Column.inherit('js.ui.DataGridDateColumn', {
+        defaults : {
+            dateFormat : 'MM-DD-YYYY',
+            parseFormat: 'YYYY-MM-DDTHH:mm:ssZ'
+        },
+        getFormatFnc: function(){
+            if(!this.$formatFnc){
+                var self = this;
+                this.$formatFnc = function (data) {
+                    if (!data) {
+                        return null;
+                    }
+                    return  moment(data, self.$.parseFormat).format(self.$.dateFormat);
+                };
+            }
+            return this.$formatFnc;
+        }
+    });
+});
