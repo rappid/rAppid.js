fca8ad0c6a45f49a2c726f103de83fa571f28c14
Marcus Krejpowicz <krebbl@gmail.com>
2012-07-24 22:30:42 +0200

    added checkbox and radiobox

---
+++ js/ui/CheckboxClass.js
@@ -0,0 +1,17 @@
+define(['js/ui/View'], function (View, DataGridColumn, List, _) {
+
+    return View.inherit('js.ui.DataGridClass', {
+        defaults: {
+            componentClass: 'checkbox',
+            selected: false,
+            value: null,
+            label: ""
+        },
+        $classAttributes: ['label'],
+        _renderLabel: function (label, oldLabel) {
+            if (label) {
+                this._renderTemplateToPlaceHolder('label', 'label', {$label: label});
+            }
+        }
+    });
+});
