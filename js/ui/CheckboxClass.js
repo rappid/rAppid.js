b0c92a4889790161be87672b0faa9a6bd046a997
Tony Findeisen <github@it-ony.de>
2012-08-04 22:20:41 +0200

    converted selected into checked in checkbox

--- js/ui/CheckboxClass.js
+++ js/ui/CheckboxClass.js
@@ -3,11 +3,12 @@ define(['js/ui/View'], function (View) {
     return View.inherit('js.ui.CheckboxClass', {
         defaults: {
             componentClass: 'checkbox',
-            selected: false,
+            checked: false,
             value: null,
             label: ""
         },
         $classAttributes: ['label', 'name'],
+
         _renderLabel: function (label, oldLabel) {
             if (label) {
                 this._renderTemplateToPlaceHolder('label', 'label', {$label: label});
