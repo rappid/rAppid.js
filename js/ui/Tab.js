3adf053bbaa065a02e06eed6c991fa74ab3cb0e8
Tony Findeisen <github@it-ony.de>
2012-08-04 22:21:15 +0200

    created tab container

---
+++ js/ui/Tab.js
@@ -0,0 +1,9 @@
+define(['js/ui/View'], function (View) {
+    return View.inherit('js.ui.Tab', {
+        $defaults: {
+            title: ""
+        },
+
+        $classAttributes: ["title"]
+    })
+});
