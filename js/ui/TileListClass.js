47d6b98284840cf7223926830d6a5fadd4875e88
Marcus Krejpowicz <krebbl@gmail.com>
2012-08-08 17:32:20 +0200

    added container for item renderer

--- js/ui/TileListClass.js
+++ js/ui/TileListClass.js
@@ -3,16 +3,44 @@ define(['js/ui/VirtualItemsView'], function(VirtualItemsView) {
     return VirtualItemsView.inherit('js.ui.TileListClass', {
 
         defaults: {
-            heightUpdatePolicy: 'in',
-            widthUpdatePolicy: 'in'
+            heightUpdatePolicy: 'both',
+            widthUpdatePolicy: 'both',
+            width: null
         },
 
-        _createRenderer: function() {
-            var ret = this.callBase();
-            ret.set({
+        _createRenderer: function(attributes) {
+            attributes = attributes || {};
+            attributes.$dataItem = null;
+
+            var ret,
+                container = this._createRenderContainer(attributes, this),
+                children = this.$templates['renderer'].createComponents(null, container);
+
+            if (container) {
+                // add all children to it
+                for (var i = 0; i < children.length; i++) {
+                    container.addChild(children[i]);
+                }
+                ret = container;
+            } else {
+                ret = children[0];
+            }
+
+            ret && ret.set({
                 position: 'absolute'
             });
+
             return ret;
+        },
+
+        _createRenderContainer: function(attributes, parentScope) {
+            var template = this.$templates['rendererContainer'];
+
+            if (template) {
+                return (template.createComponents(attributes, parentScope)[0]);
+            }
+
+            return null;
         }
     });
 
