a288558ab3511a943c957ff0a4ad3e6f3c98f788
Marcus Krejpowicz <krebbl@gmail.com>
2012-08-22 15:30:56 +0200

    removed render selected item ...

--- js/html/Select.js
+++ js/html/Select.js
@@ -9,15 +9,6 @@ define(["js/ui/SelectionView", "underscore"], function (SelectionView, _) {
             _renderMultiSelect: function (multiSelect) {
                 this.$el.multiple = multiSelect;
             },
-            _renderSelectedItems: function (items) {
-                var comp, ri;
-                // go through items, find option and set selected
-                for (var i = 0; i < this.$renderedItems.length; i++) {
-                    ri = this.$renderedItems[i];
-                    comp = ri.component;
-                    comp.set({selected: _.contains(items, ri.item)});
-                }
-            },
             _bindDomEvents: function () {
                 var self = this;
                 this.bindDomEvent('change', function (e) {
