f02213619e8da755f1f144ee854653f18c992e6c
Marcus Krejpowicz <krebbl@gmail.com>
2012-09-12 18:21:35 +0200

    added ResourceConfiguration

---
+++ js/conf/Resource.js
@@ -0,0 +1,29 @@
+define(["js/conf/Configuration"], function (Configuration) {
+
+    return Configuration.inherit('js.conf.Resource', {
+        _initializationComplete: function () {
+            this._validateConfiguration();
+            this.callBase();
+        },
+
+        _validateConfiguration: function(){
+            if (!this.$.modelClassName) {
+                throw "no modelClassName defined for '" + this.$.path + "'.";
+            }
+
+            if(!this.$.collectionClassName){
+                this.$.collectionClassName = "js.data.Collection["+this.$.modelClassName+"]";
+            }
+
+        },
+
+        getConfigurationForModelClassName: function (modelClassName) {
+            return this.getConfigurationByKeyValue("modelClassName",modelClassName);
+        },
+
+        getConfigurationForCollectionClassName: function (collectionClassName) {
+            return this.getConfigurationByKeyValue("collectionClassName", collectionClassName);
+        }
+    });
+
+});
