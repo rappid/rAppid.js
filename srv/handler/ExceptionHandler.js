41cfc3a7aa36cbfdc850e37a6a7100c983f2b456
Tony Findeisen <github@it-ony.de>
2012-09-14 16:11:22 +0200

    exceptionHandler is not responsible

--- srv/handler/ExceptionHandler.js
+++ srv/handler/ExceptionHandler.js
@@ -1,6 +1,8 @@
 define(['srv/core/Handler'], function(Handler) {
 
     return Handler.inherit('srv.core.ExceptionHandler', {
-
+        isResponsibleForRequest: function() {
+            return false;
+        }
     });
 });
