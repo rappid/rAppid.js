56e994a3035e99b0a0bf1106d95fdd40fb688408
Tony Findeisen <github@it-ony.de>
2012-09-14 10:06:26 +0200

    corrected typo and added EndPoint

---
+++ srv/core/Endpoint.js
@@ -0,0 +1,52 @@
+define(['js/core/Component'], function(Component) {
+
+    return Component.inherit('srv.core.EndPoint', {
+
+        /***
+         * starts the endpoint
+         * @param {srv.core.Server} server
+         */
+        start: function(server) {
+            this.$server = server;
+            this._start();
+            this.$started = true;
+        },
+
+        /***
+         * end point specific implementation to get started
+         * @private
+         * @abstract
+         */
+        _start: function() {
+            throw "abstract";
+        },
+
+        handleRequest: function(request, response) {
+            this.$server.handleRequest(this, request, response);
+        },
+
+        /***
+         * stops the endpoint
+         * @param callback
+         */
+        stop: function(callback) {
+            if (!this.$started) {
+                callback();
+            } else {
+                this._stop(callback);
+            }
+        },
+
+        /***
+         * end pint specific implementation to get stopped
+         * @param callback
+         * @private
+         * @abstract
+         */
+        _stop: function(callback) {
+            callback();
+        }
+
+    });
+
+});
