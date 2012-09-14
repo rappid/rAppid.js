fb314bbe3c4f2157334cee33c82ea48d71280163
Tony Findeisen <github@it-ony.de>
2012-09-14 16:09:00 +0200

    parsing the url into url info

--- srv/core/Context.js
+++ srv/core/Context.js
@@ -1,9 +1,15 @@
-define(['js/core/Base'], function (Base) {
-    return Base.inherit('srv.core.Context', {
+define(['js/core/EventDispatcher', 'url', 'querystring'], function (EventDispatcher, Url, QueryString) {
+    return EventDispatcher.inherit('srv.core.Context', {
 
-        ctor: function(request, response) {
+        ctor: function(endPoint, request, response) {
+            this.endPoint = endPoint;
             this.request = request;
             this.response = response;
+
+            var urlInfo = Url.parse(request.url);
+            request.urlInfo = urlInfo;
+            urlInfo.parameter = QueryString.parse(urlInfo.query);
+
         }
 
     })
