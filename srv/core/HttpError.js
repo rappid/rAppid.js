bbacc6dde8d2704f86cdf933d37f86cdca5745fd
Tony Findeisen <github@it-ony.de>
2012-09-16 19:27:01 +0200

    introduced HttpError

---
+++ srv/core/HttpError.js
@@ -0,0 +1,22 @@
+define([], function() {
+
+    return Error.inherit('srv.core.HttpError', {
+        ctor: function(message, statusCode, statusText) {
+            this.message = message;
+            this.statusCode = statusCode;
+            this.statusText = statusText;
+        },
+
+        toString: function() {
+            var body = this.message,
+                stack = this.stack;
+
+            if (stack) {
+                body += "<pre>" + stack + "</pre>";
+            }
+
+            return body;
+        }
+
+    });
+});
