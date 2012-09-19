1d52634b3737be651dc6c792c744569963b011d4
Marcus Krejpowicz <krebbl@gmail.com>
2012-09-19 16:20:08 +0200

    fixed configuration and addded loading of resource configuration classes on start

--- srv/handler/RestHandler.js
+++ srv/handler/RestHandler.js
@@ -1,16 +1,22 @@
-define(['require', 'srv/core/Handler', 'js/conf/DataSource', 'srv/handler/rest/ResourceRouter', 'flow'],
-    function(require, Handler, DataSourceConfiguration, ResourceRouter, flow) {
+define(['require', 'srv/core/Handler', 'js/conf/DataSource', 'js/conf/Resource', 'srv/handler/rest/ResourceRouter', 'flow', 'js/data/DataSource'],
+    function(require, Handler, DataSourceConfiguration, ResourceConfiguration, ResourceRouter, flow, DataSource) {
 
     return Handler.inherit('srv.core.RestHandler', {
 
         ctor: function() {
-            this.$dataSourceConfiguration = null;
+            this.$resourceConfiguration = null;
+            this.$dataSource = null;
+
             this.callBase();
         },
 
         addChild: function(child) {
             if (child instanceof DataSourceConfiguration) {
-                this.$dataSourceConfiguration = child;
+                this.$resourceConfiguration = child;
+            }
+
+            if (child instanceof DataSource) {
+                this.$dataSource = child;
             }
 
             this.callBase();
@@ -20,20 +26,67 @@ define(['require', 'srv/core/Handler', 'js/conf/DataSource', 'srv/handler/rest/R
             return new ResourceRouter(this);
         },
 
+        start: function(server, callback){
+            if (!this.$resourceConfiguration) {
+                callback(new Error("ResourceConfiguration missing."));
+                return;
+            }
+
+            if (!this.$dataSource) {
+                callback(new Error("DataSource missing."));
+                return;
+            }
+
+            var classes = [];
+
+            function findClasses(resourceConfiguration){
+                var config;
+                for(var i = 0; i < resourceConfiguration.$configurations.length; i++){
+                    config = resourceConfiguration.$configurations[i];
+                    if(config instanceof ResourceConfiguration){
+                        if(config.$.modelClassName){
+                            classes.push(config.$.modelClassName.replace(/\./g,'/'));
+                        }
+                        if(config.$.serverModelClassName){
+                            classes.push(config.$.serverModelClassName.replace(/\./g, '/'));
+                        }
+                        if(config.$.resourceClassName){
+                            classes.push(config.$.resourceClassName.replace(/\./g, '/'));
+                        }
+                        findClasses(config);
+                    }
+                }
+            }
+
+            findClasses(this.$resourceConfiguration);
+
+
+            require(classes, function() {
+                callback();
+            }, function(err) {
+                callback(err);
+            });
+
+        },
+
+        /***
+         *
+         * @param {srv.core.Context} context
+         * @return {*}
+         */
+        getDataSource: function(context) {
+            return this.$dataSource;
+        },
+
         handleRequest: function(context, callback) {
 
             var self = this;
 
-            if (!this.$dataSourceConfiguration) {
-                throw new Error("DataSourceConfiguration missing.");
-            }
-
-
             flow()
-                .seq("resource", function(cb) {
+                .seq("resource", function (cb) {
                     self._getResourceRouter().getResource(context, cb);
                 })
-                .seq(function(cb) {
+                .seq(function (cb) {
                     this.vars["resource"].handleRequest(context, cb);
                 })
                 .exec(callback);
