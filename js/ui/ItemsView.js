e41035980f099910f15e3ffe8056449ec004a6db
Marcus Krejpowicz <krebbl@gmail.com>
2012-09-24 15:43:04 +0200

    added method comments and refactored binding on paths

--- js/ui/ItemsView.js
+++ js/ui/ItemsView.js
@@ -18,12 +18,14 @@ define(
             ctor: function(){
                 this.callBase();
 
-                this.bind(['items','sort'], this._onSort, this);
-                this.bind(['items','reset'], this._onReset, this);
-                this.bind(['items','add'], this._onItemAdd, this);
-                this.bind(['items','remove'], this._onItemRemove, this);
+                this.bind('items','sort', this._onSort, this);
+                this.bind('items','reset', this._onReset, this);
+                this.bind('items','add', this._onItemAdd, this);
+                this.bind('items','remove', this._onItemRemove, this);
             },
-
+            /***
+             * Returns true if items are available
+             */
             hasItems: function () {
                 if (this.$.items) {
                     return this.$.items.length;
@@ -75,7 +77,12 @@ define(
                     this._removeRenderedItem(e.$.item);
                 }
             },
-
+            /**
+             * Inner render method for a list of items
+             * All rendered items will be removed and destroyed before rendering
+             * @param {Array} items[]
+             * @private
+             */
             _innerRenderItems: function (items) {
                 if (this.$renderedItems) {
                     var c;
@@ -98,26 +105,49 @@ define(
                 }
 
             },
-
-            _createComponentForItem: function (item, i) {
+            /***
+             * Creates a component based on the template for a given item
+             * @param {Object} item
+             * @param {Number} index
+             * @return {js.core.Component} component
+             * @private
+             */
+            _createComponentForItem: function (item, index) {
                 var attr = {};
                 attr[this._getItemKey()] = item;
-                attr[this._getIndexKey()] = i;
-                var comp = this.$templates['item'].createComponents(attr)[0];
-
+                attr[this._getIndexKey()] = index;
+                return this.$templates['item'].createComponents(attr)[0];
+            },
+            /***
+             * Caches the component to a given item
+             * @param {Object} item
+             * @param {js.core.Component} component
+             * @return {js.core.Component} returns the component
+             * @private
+             */
+            _cacheComponentForItem: function(item,component){
                 var key = this._getKeyForItem(item);
-                if(key){
-                    this.$renderedItemsMap[key] = comp;
-                }else{
+                if (key) {
+                    this.$renderedItemsMap[key] = component;
+                } else {
                     // add to rendered item map
                     this.$renderedItems.push({
                         item: item,
-                        component: comp
+                        component: component
                     });
                 }
-
-                return comp;
+                return component;
             },
+            /***
+             * Calculates a cache key for a given item.
+             * If a keyPath is set, it will return the value of the path
+             * Otherwise if the item is a {js.core.Bindable} it will return the $cid
+             * If no keyPath is set and it's an object it will return null;
+             *
+             * @param item
+             * @return {*}
+             * @private
+             */
             _getKeyForItem: function(item){
                 if (item instanceof Bindable) {
                     var key;
@@ -126,22 +156,47 @@ define(
                     } else {
                         return item.$cid;
                     }
+                } else {
+                    if(this.$.keyPath && item instanceof Object){
+                        return this.get(item, this.$.keyPath);
+                    }
                 }
-                // TODO: handle hash objects
                 return null;
             },
-            _innerRenderItem: function (item, i) {
-                this.addChild(this._createComponentForItem(item, i));
+            /***
+             * Inner render method for an item
+             * Creates a component, caches the component and adds the component to the list of children
+             * @param {Object} item
+             * @param {Number} index
+             * @private
+             */
+            _innerRenderItem: function (item, index) {
+                var component = this._createComponentForItem(item, index);
+                this._cacheComponentForItem(item,component);
+                this.addChild(component);
             },
 
+            /***
+             * Returns the key to access the item in the template
+             * @return {String}
+             * @private
+             */
             _getItemKey: function () {
                 return "$" + this.$.itemKey;
             },
-
+            /***
+             * Returns the key to access the index in the template
+             * @return {String}
+             * @private
+             */
             _getIndexKey: function () {
                 return "$" + this.$.indexKey;
             },
-
+            /***
+             * Removes an item from the list
+             * @param {Object} item
+             * @private
+             */
             _removeRenderedItem: function (item) {
                 var ri;
                 var key = this._getKeyForItem(item);
@@ -163,7 +218,11 @@ define(
                     }
                 }
             },
-
+            /**
+             * Returns the rendered component to a given item
+             * @param item
+             * @return {js/core/Component} component
+             */
             getComponentForItem: function (item) {
                 var key = this._getKeyForItem(item);
                 if(key){
