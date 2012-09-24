59cec5873ac0a8f8412f7e8473b25cea241cbda4
Marcus Krejpowicz <krebbl@gmail.com>
2012-09-24 19:02:36 +0200

    now the itemKey and indexkey are added to classAttributes ...

--- js/ui/ListView.js
+++ js/ui/ListView.js
@@ -14,6 +14,8 @@ define(['js/ui/ItemsView', 'js/html/HtmlElement'], function(ItemsView, HtmlEleme
                 tagName: 'li'
             });
 
+            component.$classAttributes.push(this.$.itemKey, this.$.indexKey);
+
             for (var j = 0; j < components.length; j++) {
                 component.addChild(components[j]);
             }
