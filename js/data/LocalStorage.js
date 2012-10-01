39586701a702a558972c771cafd7f166429498f7
Tony Findeisen <github@it-ony.de>
2012-10-01 17:34:44 +0200

    abstracted LocalStorage to run on node

--- js/data/LocalStorage.js
+++ js/data/LocalStorage.js
@@ -1,57 +1,114 @@
-define(['js/core/Component'], function(Component) {
+define(['js/core/Component' , 'inherit'], function (Component, inherit) {
 
     /***
      * Local Storage implementation
      */
-    return Component.inherit('js.data.LocalStorage', {
-        ctor: function(){
-            this.callBase();
-            var window = this.$stage.$window, document = this.$stage.$document;
-            if(window.localStorage){
-                this.$localStorage = window.localStorage;
-            }else{
-                this.$localStorage = {
-                    getItem: function (sKey) {
-                        if (!sKey || !this.hasOwnProperty(sKey)) {
-                            return null;
-                        }
-                        return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
-                    },
-                    key: function (nKeyId) {
-                        return unescape(document.cookie.replace(/\s*\=(?:.(?!;))*$/, "").split(/\s*\=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
-                    },
-                    setItem: function (sKey, sValue) {
-                        if (!sKey) {
-                            return;
-                        }
-                        document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
-                        this.length = document.cookie.match(/\=/g).length;
-                    },
-                    length: 0,
-                    removeItem: function (sKey) {
-                        if (!sKey || !this.hasOwnProperty(sKey)) {
-                            return;
-                        }
-                        document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
-                        this.length--;
-                    },
-                    hasOwnProperty: function (sKey) {
-                        return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
-                    }
-                };
-                this.$localStorage.length = (document.cookie.match(/\=/g) || window.localStorage).length;
-            }
+    var LocalStorage = Component.inherit('js.data.LocalStorage', {
 
+
+        ctor: function () {
+
+            this.$implementation = null;
+            this.callBase();
+
+            if (this.runsInBrowser()) {
+                var window = this.$stage.$window,
+                    document = this.$stage.$document;
+
+                if (window.localStorage) {
+                    this.$implementation = window.localStorage;
+                } else {
+                    this.$implementation = new LocalStorage.CookieImplementation(document);
+                }
+            } else {
+                this.$implementation = new LocalStorage.ObjectImplementation();
+            }
         },
-        getItem: function(key){
-            return this.$localStorage.getItem(key);
+
+        getItem: function (key) {
+            return this.$implementation.getItem(key);
         },
-        setItem: function(key, value){
-            this.$localStorage.setItem(key,value);
+
+        setItem: function (key, value) {
+            this.$implementation.setItem(key, value);
         },
-        removeItem: function(key){
-            this.$localStorage.removeItem(key);
+
+        removeItem: function (key) {
+            this.$implementation.removeItem(key);
+        },
+
+        clear: function() {
+            this.$implementation.clear();
+        }
+
+    });
+
+    // TODO: implement length as getter
+
+    LocalStorage.CookieImplementation = inherit({
+
+        getItem: function (sKey) {
+            if (!sKey || !this.hasOwnProperty(sKey)) {
+                return null;
+            }
+            return unescape(document.cookie.replace(new RegExp("(?:^|.*;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*((?:[^;](?!;))*[^;]?).*"), "$1"));
+        },
+
+        key: function (nKeyId) {
+            return unescape(document.cookie.replace(/\s*=(?:.(?!;))*$/, "").split(/\s*=(?:[^;](?!;))*[^;]?;\s*/)[nKeyId]);
+        },
+
+        setItem: function (sKey, sValue) {
+            if (!sKey) {
+                return;
+            }
+            document.cookie = escape(sKey) + "=" + escape(sValue) + "; expires=Tue, 19 Jan 2038 03:14:07 GMT; path=/";
+            this.length = document.cookie.match(/=/g).length;
+        },
+
+        length: 0,
+
+        size: function() {
+            this.$implementation.length = document.cookie.match(/=/g).length;
+        },
+
+        removeItem: function (sKey) {
+            if (!sKey || !this.hasOwnProperty(sKey)) {
+                return;
+            }
+            document.cookie = escape(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+            this.length--;
+        },
+        hasOwnProperty: function (sKey) {
+            return (new RegExp("(?:^|;\\s*)" + escape(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
         }
     });
+
+    // TODO: implement length as getter
+
+    LocalStorage.ObjectImplementation = inherit({
+
+        ctor: function() {
+            this.clear();
+        },
+
+        getItem: function (key) {
+            return this.$storage[key];
+        },
+
+        setItem: function (key, value) {
+            this.$storage[key] = value;
+        },
+
+        removeItem: function (key) {
+            delete this.$storage[key];
+        },
+
+        clear: function() {
+            this.$storage = {};
+        }
+    });
+
+    return LocalStorage;
 
 });
