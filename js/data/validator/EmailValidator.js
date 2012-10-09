7cd079d952cdb34da0add5de1967d760ccfc4ca9
Marcus Krejpowicz <krebbl@gmail.com>
2012-10-09 12:05:43 +0200

    added todo

--- js/data/validator/EmailValidator.js
+++ js/data/validator/EmailValidator.js
@@ -2,6 +2,7 @@ define(['js/data/validator/RegExValidator'], function (RegExValidator) {
 
     return RegExValidator.inherit('js.data.validator.EmailValidator', {
         defaults: {
+            // TODO: add umlauts
             regEx: /^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
             errorCode: 'emailError'
         }
