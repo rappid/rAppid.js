f8957cbd834ff6e2425d8cc9ca0bcd875e6ae4d7
Tony Findeisen <github@it-ony.de>
2012-10-12 18:37:54 +0200

    added start and stop to filter

--- srv/core/Filter.js
+++ srv/core/Filter.js
@@ -1,12 +1,25 @@
-define(['js/core/Base'], function(Base) {
+define(['js/core/Component'], function (Base) {
 
     return Base.inherit('srv.core.Filter', {
 
-        beginRequest: function(context, callback) {
+        start: function(server, callback) {
+            this.$server = server;
             callback();
         },
 
-        endRequest: function(context, callback) {
+        stop: function(callback) {
+            callback();
+        },
+
+        beginRequest: function (context, callback) {
+            callback();
+        },
+
+        beforeHeadersSend: function (context, callback) {
+            callback();
+        },
+
+        endRequest: function (context, callback) {
             callback();
         }
 
