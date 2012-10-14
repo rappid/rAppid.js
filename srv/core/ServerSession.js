cc538bdb6a62b9e0ca55074b5904730fc2fe0f6c
Tony Findeisen <github@it-ony.de>
2012-10-14 22:07:05 +0200

    added start method to session

--- srv/core/ServerSession.js
+++ srv/core/ServerSession.js
@@ -2,6 +2,25 @@ define(['js/data/Model'], function(Model) {
     return Model.inherit('srv.core.ServerSession', {
          schema: {
              expires: Date
-         }
+         },
+
+        start: function(callback) {
+            var self = this;
+
+            if (this.$.id) {
+                // session with session id
+                this.fetch(null, function (err) {
+                    if (err) {
+                        self.log("Couldn't fetch session.", "warn");
+                    }
+                    self.$started = true;
+                    callback();
+                });
+            } else {
+                this.$started = true;
+                this.set("id", this.$sessionFilter.generateSessionId());
+                callback();
+            }
+        }
     });
 });
