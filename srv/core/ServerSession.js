b54d9ee3a78babb17549ef80e4d2bc58a94a1613
Marcus Krejpowicz <krebbl@gmail.com>
2012-10-15 13:57:20 +0200

    fixed session clearing

--- srv/core/ServerSession.js
+++ srv/core/ServerSession.js
@@ -1,25 +1,41 @@
-define(['js/data/Model'], function(Model) {
+define(['js/data/Model'], function (Model) {
     return Model.inherit('srv.core.ServerSession', {
-         schema: {
-             expires: Date
-         },
+        schema: {
+            id: String,
+            expires: Date
+        },
 
-        start: function(callback) {
+        start: function (callback) {
             var self = this;
 
+
             if (this.$.id) {
-                // session with session id
+                this.sessionId = this.$.id;
+                // fetch session with session id
                 this.fetch(null, function (err) {
                     if (err) {
+                        // if session couldn't be fetched
                         self.log("Couldn't fetch session.", "warn");
+                        // set session as new
+                        self.set('id', undefined);
+                    }else if(self.$.expires.getTime() < (new Date()).getTime()){
+                        // if session is expired
+                        // clear session data
+                        self.clear();
+                        // and set old session id
+                        self.set('id',self.sessionId);
                     }
-                    self.$started = true;
-                    callback();
+
+                    internalCallBack();
                 });
             } else {
-                this.$started = true;
-                this.set("id", this.$sessionFilter.generateSessionId());
-                callback();
+                this.sessionId = this.$sessionFilter.generateSessionId();
+                internalCallBack();
+            }
+
+            function internalCallBack(err){
+                self.$started = true;
+                callback(err);
             }
         }
     });
