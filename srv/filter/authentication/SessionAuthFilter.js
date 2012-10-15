853870686857557b0acec2ecbcf9262ab2c21d07
Marcus Krejpowicz <krebbl@gmail.com>
2012-10-15 16:46:36 +0200

    added SessionAuthFilter

---
+++ srv/filter/authentication/SessionAuthFilter.js
@@ -0,0 +1,18 @@
+define(['srv/core/AuthenticationFilter'], function (AuthenticationFilter) {
+
+    return AuthenticationFilter.inherit('srv.filter.PlainAuthFilter', {
+        defaults: {
+            key: 'userId'
+        },
+
+        beginRequest: function (context, callback) {
+            var userId = context.session.get(this.$.key);
+            if(userId){
+                var data = {};
+                data[this.$.key] = data;
+                this._createAuthenticationRequest(data);
+            }
+            callback();
+        }
+    })
+});
