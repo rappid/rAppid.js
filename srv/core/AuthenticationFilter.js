6c8b03c161376eac55f3b52e83701645cd1bba25
Tony Findeisen <github@it-ony.de>
2012-10-15 23:45:40 +0200

    added handleAuthenticationRequest to AuthenticationFilter

--- srv/core/AuthenticationFilter.js
+++ srv/core/AuthenticationFilter.js
@@ -1,4 +1,5 @@
-define(['srv/core/Filter', 'require', 'flow', 'js/data/DataSource', 'srv/core/ServerSession', 'srv/core/AuthenticationProvider'], function (Filter, require, flow, DataSource, ServerSession, AuthenticationProvider) {
+define(['srv/core/Filter', 'require', 'flow', 'js/data/DataSource', 'srv/core/ServerSession', 'srv/core/AuthenticationProvider'],
+    function (Filter, require, flow, DataSource, ServerSession, AuthenticationProvider) {
 
     return Filter.inherit('srv.core.AuthenticationFilter', {
 
@@ -7,11 +8,31 @@ define(['srv/core/Filter', 'require', 'flow', 'js/data/DataSource', 'srv/core/Se
         },
 
         _start: function (callback) {
-            if (!(this.$.authenticationProvider instanceof AuthenticationProvider)) {
-                callback(new Error("AuthenticationProvider not instanceof AuthenticationProvider"));
-            }else{
-                callback();
-            }
+            // FIXME
+//            if (!this.$.authenticationProvider || !(this.$.authenticationProvider instanceof AuthenticationProvider)) {
+//                callback(new Error("AuthenticationProvider not instanceof AuthenticationProvider"));
+//            } else {
+//                callback();
+//            }
+            callback();
+        },
+
+        /***
+         * @abstract
+         * @param context
+         * @return {Boolean}
+         */
+        isResponsibleForAuthenticationRequest: function(context) {
+            return false;
+        },
+
+        /***
+         *
+         * @param context
+         * @param callback
+         */
+        handleAuthenticationRequest: function(context, callback) {
+            throw new Error("Not implemented");
         },
 
         _createAuthenticationRequest: function(context, data){
