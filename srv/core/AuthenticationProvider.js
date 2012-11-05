bd00b5db8f453b26b042b9979071f5d31d6f535b
Tony Findeisen <github@it-ony.de>
2012-11-05 22:02:00 +0100

    introduced authenticateByToken and authenticateByData

--- srv/core/AuthenticationProvider.js
+++ srv/core/AuthenticationProvider.js
@@ -1,23 +1,39 @@
-define(['js/core/Component'], function (Component) {
+define(['js/core/Component', 'srv/core/HttpError'], function (Component, HttpError) {
 
     return Component.inherit('srv.core.AuthenticationProvider', {
 
-        start: function(server, callback) {
+        start: function (server, callback) {
             this.$server = server;
             this._start(callback);
         },
 
-        _start: function(callback) {
+        _start: function (callback) {
             callback();
         },
 
-        stop: function(callback) {
+        stop: function (callback) {
             callback();
         },
 
-       authenticate: function(authenticationData, callback){
-           callback();
-       }
+        authenticate: function (authentication, callback) {
+            if (authentication.isAuthenticationByToken()) {
+                this._authenticateByToken(authentication, callback);
+            } else {
+                this._authenticateByData(authentication, callback);
+            }
+        },
+
+        _authenticateByToken: function (authentication, callback) {
+            callback(new Error("AuthenticateByToken not implemented"));
+        },
+
+        _authenticateByData: function (authentication, callback) {
+            callback(new Error("AuthenticateByData not implemented"));
+        },
+
+        _createAuthenticationError: function(message) {
+            return new HttpError(message, 400);
+        }
 
     });
 });
