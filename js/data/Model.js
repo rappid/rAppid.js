133a2314c4884bcc95551685b989b005045bf8bd
Marcus Krejpowicz <krebbl@gmail.com>
2012-11-30 15:14:05 +0100

    fixed get collection

--- js/data/Model.js
+++ js/data/Model.js
@@ -102,7 +102,7 @@ define(["js/data/Entity", "js/core/List", "flow", "underscore"], function (Entit
             }
             var collection = this.get(key);
             if (!collection) {
-                var context = this.getContextForChild(schemaDefinition.type.$modelFactory);
+                var context = this.getContextForChild(schemaDefinition.type.prototype.$modelFactory);
                 if (context) {
                     collection = context.createCollection(schemaDefinition.type, null);
                     collection.$parent = this;
