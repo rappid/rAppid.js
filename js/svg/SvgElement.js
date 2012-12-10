dbbb43768e4f834812b44cfc2621dc1d85993ab0
Tony Findeisen <github@it-ony.de>
2012-12-10 20:01:53 +0100

    refactored SvgElement completely

--- js/svg/SvgElement.js
+++ js/svg/SvgElement.js
@@ -1,71 +1,120 @@
-define(['js/core/DomElement'], function(DomElement) {
+define(['js/core/DomElement', 'js/core/List', 'js/core/Bindable'], function (DomElement, List, Bindable) {
 
     var SvgElement = DomElement.inherit("js.svg.SvgElement", {
 
-        ctor: function() {
+        defaults: {
+            transformations: List,
+            _transform: null,
+
+            translateX: null,
+            translateY: null
+        },
+
+        $classAttributes: ["transformations", "translateX", "translateY"],
+
+        ctor: function () {
             // default namespace
             this.$namespace = SvgElement.SVG_NAMESPACE;
-            this.$transformations = [];
+            this.callBase();
+
+            this.bind("transformations", "all", this._refreshTransform, this);
+        },
+
+        transform: function(transform) {
+            if (this.$transform) {
+                this.$transform.set("transform", transform);
+            } else {
+                this.$transform = new SvgElement.Transform({
+                    transform: transform
+                });
+                this.$.transformations.add(this.$transform);
+            }
+
+            return this;
+        },
+
+        _commitChangedAttributes: function ($) {
+            if (this._hasSome($, ["x", "y"])) {
+                var x = this.$.translateX;
+                var y = this.$.translateY;
+
+                (x || y) && this.translate(x, y);
+            }
 
             this.callBase();
         },
 
-        resetTransform: function() {
-            if (this.$transformation.length) {
-                this.$transformation = [];
-                this._refreshTransform();
-            }
-        },
+        translate: function (x, y) {
 
-        addTransform: function(transform) {
-            if (transform) {
-                this.$transformations.push(transform);
-                this._refreshTransform();
+            if (this.$translate) {
+                this.$translate.set({
+                    x: x,
+                    y: y
+                });
+            } else {
+                this.$translate = new SvgElement.Translate({
+                    x: x,
+                    y: y
+                });
+                this.$.transformations.add(this.$translate);
             }
-        },
 
-        translate: function(x, y) {
-            if (x && y) {
-                this.addTransform(new SvgElement.Translate(x, y));
-            }
             return this;
         },
 
-        rotate: function(r, x, y) {
-            this.addTransform(new SvgElement.Rotate(r, x, y));
+        rotate: function (r, x, y) {
+            if (this.$rotate) {
+                this.$rotate.set({
+                    r: r,
+                    x: x,
+                    y: y
+                });
+            } else {
+                this.$rotate = new SvgElement.Translate({
+                    r: r,
+                    x: x,
+                    y: y
+                });
+                this.$.transformations.add(this.$rotate);
+            }
+            
             return this;
         },
 
-        _refreshTransform: function() {
+        _refreshTransform: function () {
 
+            
             var transformations = [];
-            for (var i = 0; i < this.$transformations.length; i++) {
-                var t = this.$transformations[i];
 
-                if (t instanceof SvgElement.Transform) {
+            this.$.transformations.each(function(t) {
+                if (t instanceof SvgElement.TransformBase) {
                     transformations.push(t.toString());
                 } else {
                     transformations.push(t);
                 }
-            }
+            });
 
-            this.set('transform', transformations.join(" "));
+            this.set('_transform', transformations.join(" "));
+        },
+
+        _renderTransform: function() {
+            // transforms needs to be set via transformations
+        },
+
+        _render_transform: function(transform) {
+            this._setAttribute("transform", transform);
         },
 
         // render each href Element in xlink namespace
-        _renderHref: function(href, oldHref) {
+        _renderHref: function (href, oldHref) {
             this._setAttribute("href", href, SvgElement.XLINK_NAMESPACE);
-        },
-
-        _renderClass: function(value) {
-            this._setAttribute('class', value);
         }
     });
 
     SvgElement.XLINK_NAMESPACE = "http://www.w3.org/1999/xlink";
     SvgElement.SVG_NAMESPACE = "http://www.w3.org/2000/svg";
 
-    SvgElement.Matrix = function(a, b, c, d, e, f) {
+    SvgElement.Matrix = function (a, b, c, d, e, f) {
         if (arguments.length) {
             this.a = a;
             this.b = b;
@@ -83,27 +132,36 @@ define(['js/core/DomElement'], function(DomElement) {
         }
     };
 
-    SvgElement.Transform = function() {
-    };
+    SvgElement.TransformBase = Bindable.inherit('js.svg.SvgElement.TransformBase', {
+    });
 
-    SvgElement.Translate = SvgElement.Transform.inherit({
-        ctor: function(x, y) {
-            this.x = x;
-            this.y = y;
+    SvgElement.Transform = SvgElement.TransformBase.inherit('js.svg.SvgElement.TransformBase', {
+        defaults: {
+            transform: null
         },
+
         toString: function() {
-            return "translate(" + this.x + "," + this.y + ")";
+            return this.$.transform || "";
         }
     });
 
-    SvgElement.Rotate = SvgElement.Transform.inherit({
-        ctor: function(r, x, y) {
-            this.r = r || 0;
-            this.x = x || 0;
-            this.y = y || 0;
+    SvgElement.Translate = SvgElement.TransformBase.inherit("js.svg.SvgElement.Translate", {
+        toString: function () {
+            return "translate(" + (this.$.x || 0) + "," + (this.$.y || 0) + ")";
+        }
+    });
+
+
+    SvgElement.Rotate = SvgElement.TransformBase.inherit("js.svg.SvgElement.Rotate", {
+
+        defaults: {
+            r: 0,
+            x: 0,
+            y: 0
         },
-        toString: function() {
-            return "rotate(" + this.r + "," + this.x + "," + this.y + ")"
+
+        toString: function () {
+            return "rotate(" + (this.$.r || 0) + "," + (this.$.x || 0) + "," + (this.$.y || 0) + ")";
         }
     });
 
