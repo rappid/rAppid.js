2d8420bf71b23da6db99cfad1eefad815787d10c
Tony Findeisen <github@it-ony.de>
2013-01-08 13:30:09 +0100

    added save method for SVG to save as an Image

--- js/svg/Svg.js
+++ js/svg/Svg.js
@@ -29,6 +29,26 @@ define(['js/svg/SvgElement'], function (SvgElement) {
                 x: this.$viewBoxWidth / this.$.width,
                 y: this.$viewBoxHeight / this.$.height
             }
+        },
+
+        save: function() {
+
+            var window = this.$stage.$window;
+
+            // dirty hack to get dom as string
+            var div = this.$stage.$document.createElement("div");
+            div.appendChild(this.$el.cloneNode(true));
+            // dirty hack2
+            var svgContent = div.innerHTML.replace(/^\<svg/, "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' ");
+            svgContent = svgContent.replace(/(href=['"])\/{2}/g, "$1http://");
+
+            var svg = new Blob([svgContent], {
+                type: "image/svg+xml;charset=utf-8"
+            });
+
+            var domUrl = window.URL || window.webkitURL || window;
+            return domUrl.createObjectURL(svg);
+
         }
 
     });
