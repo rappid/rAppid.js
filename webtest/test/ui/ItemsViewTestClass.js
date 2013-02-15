a7961047c48ae90efc9fdca60d40affd69f032ce
Marcus Krejpowicz <krebbl@gmail.com>
2013-02-15 16:42:44 +0100

    added ItemsViewTests

---
+++ webtest/test/ui/ItemsViewTestClass.js
@@ -0,0 +1,41 @@
+define(["js/core/Application", "js/core/List"], function (Application, List) {
+    return Application.inherit({
+        defaults: {
+            items: null
+        },
+
+        ctor: function () {
+
+            this.callBase();
+
+            this.set('items', new List([
+                {
+                    id: 1,
+                    value: "A"
+                },
+                {
+                    id: 2,
+                    value: "B"
+                }
+            ]));
+
+            this.set('woTemplateItems', new List([
+                {
+                    value: "A"
+                },
+                {
+                    value: "B"
+                }
+            ]));
+        },
+        addItemAt: function (item, index) {
+            this.$.items.add(item, {index: index});
+        },
+        removeItemAt: function(index){
+            this.$.items.removeAt(index);
+        },
+        resetItems: function(){
+            this.$.items.reset();
+        }
+    });
+});
