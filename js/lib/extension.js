c995abf257fdb002821597333a4f337571a2da0f
Tony Findeisen <github@it-ony.de>
2013-03-08 13:56:23 +0100

    added polyfill for window.getComputedStyles

--- js/lib/extension.js
+++ js/lib/extension.js
@@ -16,5 +16,25 @@ define([],function(){
         };
     }
 
+    if (typeof window !== "undefined" && !window.getComputedStyle) {
+        window.getComputedStyle = function (el) {
+            this.el = el;
+            this.getPropertyValue = function (prop) {
+                var re = /(\-([a-z]){1})/g;
+                if (prop == 'float') prop = 'styleFloat';
+                if (re.test(prop)) {
+                    prop = prop.replace(re, function () {
+                        return arguments[2].toUpperCase();
+                    });
+                }
+                return el.currentStyle[prop] ? el.currentStyle[prop] : null;
+            };
+
+            return this;
+        }
+    }
+
+
+
     return true;
 });
