0750725454eb5a54a6f9c136b60434c73e0b4244
Tony Findeisen <github@it-ony.de>
2013-03-08 14:24:36 +0100

    removed useless textContent Code

--- js/core/Component.js
+++ js/core/Component.js
@@ -434,11 +434,6 @@ define(["require", "js/core/Element", "js/core/TextElement", "js/core/Bindable",
                         return this.$stage.$applicationContext.createInstance(fqClassName, [attributes, node, this.$stage, this, rootScope], className);
 
                     } else if (node.nodeType == 3 || node.nodeType == 4) { // Text nodes
-                        // remove whitespaces from text text nodes
-                        var text = node.textContent ? node.textContent : node.text;
-                        if (!node.textContent) {
-                            node.textContent = text;
-                        }
                         // only instantiation and construction but no initialization
                         return this._createTextElement(node, rootScope);
                     }
