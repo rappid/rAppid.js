8d1c4c492000fdee08229a3e1ca1e3fb4d69bed0
Tony Findeisen <github@it-ony.de>
2013-03-10 18:51:51 +0100

    corrected entity tests to be asynchronous, added optional attribute tests

--- test/EntityTest.js
+++ test/EntityTest.js
@@ -1,6 +1,7 @@
 var chai = require('chai'),
     should = chai.should(),
     expect = chai.expect,
+    flow = require("flow.js").flow,
     _ = require('underscore'),
     testRunner = require('..').TestRunner.setup();
 
@@ -9,18 +10,37 @@ var C = {};
 
 describe('js.data.Entity', function () {
 
-    var EntityClass;
+    var EntityClass,
+        OptionalAttributeEntity;
 
     before(function (done) {
         testRunner.requireClasses({
             Entity: 'js/data/Entity',
-            List: 'js/core/List'
+            List: 'js/core/List',
+            RegExValidator: 'js/data/validator/RegExValidator'
         }, C, function(err) {
 
             EntityClass = C.Entity.inherit('app.entity.Entity', {
                 schema: {
                     name: String
                 }
+            });
+
+            OptionalAttributeEntity = EntityClass.inherit("app.entity.OptionalAttributeEntity", {
+                schema: {
+                    phone: {
+                        required: false,
+                        type: String
+                    }
+                },
+
+                validators: [
+                    new C.RegExValidator({
+                        field: "phone",
+                        regEx: /\d+/,
+                        errorMessage: "Phone number in wrong format"
+                    })
+                ]
             });
 
             done(err);
@@ -30,17 +50,82 @@ describe('js.data.Entity', function () {
 
     describe('#validate', function () {
 
-        it('should validate required fields', function () {
+        it('should validate required fields', function (done) {
             var entity = new EntityClass();
 
-            entity.validate({}, function (err) {
-                expect(err).not.to.exist;
-                expect(entity.isValid()).to.be.equal(false);
-                expect(entity.fieldError('name')).to.exist;
+            entity.validate(null, function(err) {
+                var error = null;
+
+                try {
+                    expect(err).not.to.exist;
+                    expect(entity.isValid()).to.be.equal(false);
+                    expect(entity.fieldError('name')).to.exist;
+                } catch (e) {
+                    error = e;
+                }
+
+                done(error);
             });
+
         });
 
-        it('should validate the types of the attributes', function () {
+        it ('should handle optional fields', function(done) {
+            var entity = new OptionalAttributeEntity({
+                name: "foo"
+            });
+
+            entity.validate(null, done);
+
+        });
+
+        it('optional fields with wrong format should not validate', function (done) {
+            var entity = new OptionalAttributeEntity({
+                name: "foo",
+                phone: "a"
+            });
+
+            entity.validate(null, function(err) {
+
+                var error;
+
+                try {
+                    expect(err).to.not.exist;
+                    expect(entity.fieldError('phone')).to.exist;
+                } catch (e) {
+                    error = e;
+                }
+
+                done(error);
+
+            });
+
+        });
+
+        it('optional fields with correct format should validate', function (done) {
+            var entity = new OptionalAttributeEntity({
+                name: "foo",
+                phone: "123"
+            });
+
+            entity.validate(null, function (err) {
+
+                var error;
+
+                try {
+                    expect(err).to.not.exist;
+                    expect(entity.isValid()).to.be.true;
+                } catch (e) {
+                    error = e;
+                }
+
+                done(error);
+
+            });
+
+        });
+
+
+        it('should validate the types of the attributes', function (done) {
             EntityClass = C.Entity.inherit('app.entity.Entity', {
                 schema: {
                     name: String,
@@ -59,19 +144,28 @@ describe('js.data.Entity', function () {
                 subEntity: "asd"
             });
 
-            entity.validate({}, function (err) {
-                expect(err).not.to.exist;
-                expect(entity.isValid()).to.be.equal(false);
-                expect(entity.fieldError('name')).to.exist;
-                expect(entity.fieldError('isMale')).to.exist;
-                expect(entity.fieldError('birthDate')).to.exist;
-                expect(entity.fieldError('age')).to.exist;
-                expect(entity.fieldError('subEntity')).to.exist;
+            entity.validate(null, function (err) {
+
+                var error = null;
+
+                try {
+                    expect(err).not.to.exist;
+                    expect(entity.isValid()).to.be.equal(false);
+                    expect(entity.fieldError('name')).to.exist;
+                    expect(entity.fieldError('isMale')).to.exist;
+                    expect(entity.fieldError('birthDate')).to.exist;
+                    expect(entity.fieldError('age')).to.exist;
+                    expect(entity.fieldError('subEntity')).to.exist;
+                } catch (e) {
+                    error = e;
+                }
+
+                done(error);
             });
 
         });
 
-        it('should validate sub entity', function(){
+        it('should validate sub entity', function(done){
             var SubEntityClass = C.Entity.inherit('app.entity.SubEntity', {
                 schema: {
                     name: String
@@ -86,18 +180,27 @@ describe('js.data.Entity', function () {
             });
 
             var subEntity  = new SubEntityClass({
-
             });
+
             var entity = new EntityClass({
                 name: "Test",
                 subEntity: subEntity
             });
 
-            entity.validate({}, function(err){
-                expect(err).not.to.exist;
-                expect(entity.isValid()).to.be.equal(false);
-                expect(entity.fieldError('name')).not.to.exist;
-                expect(entity.fieldError('subEntity')).to.exist;
+            entity.validate(null, function(err){
+
+                var error = null;
+
+                try {
+                    expect(err).not.to.exist;
+                    expect(entity.isValid()).to.be.equal(false);
+                    expect(entity.fieldError('name')).not.to.exist;
+                    expect(entity.fieldError('subEntity')).to.exist;
+                } catch (e) {
+                    error = e;
+                }
+
+                done(error);
             });
         });
 
