1d3d70fbc358d8198bca4656954e3123e7207275
Marcus Krejpowicz <krebbl@gmail.com>
2013-03-10 14:19:12 +0100

    added item binding for DataGridRow

--- js/ui/DataGridRow.js
+++ js/ui/DataGridRow.js
@@ -3,7 +3,8 @@ define(['js/ui/ItemsView'], function (ItemsView) {
     return ItemsView.inherit('js.ui.DataGridRow', {
         defaults: {
             tagName: "tr",
-            itemKey: "column"
+            itemKey: "column",
+            item: "{$dataItem.data}"
         },
         _renderWidth: function(){
             // do nothing
