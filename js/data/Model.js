4a51bf73dc62b7b12340c9ee5326efd83b51c2f8
Marcus Krejpowicz <krebbl@gmail.com>
2013-03-10 14:18:36 +0100

    fixed cloning of subModels

--- js/data/Model.js
+++ js/data/Model.js
@@ -233,7 +233,15 @@ define(["js/data/Entity", "js/core/List", "flow", "underscore"], function (Entit
 
         isCreated: function() {
             return this._status() === STATE.CREATED;
-        }.onChange('id')
+        }.onChange('id'),
+
+        _cloneAttribute: function (value) {
+            // if the attribute is a model, just link it and don't clone
+            if(value instanceof Model){
+                return value;
+            }
+            return this.callBase();
+        }
 
     });
 
