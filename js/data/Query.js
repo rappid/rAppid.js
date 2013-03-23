54246c98219a5f8f14ac2a7fb1af00c35fcb5069
Marcus Krejpowicz <krebbl@gmail.com>
2013-03-23 19:17:04 +0100

    removed array executor from query

--- js/data/Query.js
+++ js/data/Query.js
@@ -1,37 +1,7 @@
-define(['Query', 'js/lib/query/ArrayExecutor', 'js/core/Bindable'], function(Query, ArrayExecutor, Bindable){
-
-
-    var Executor = ArrayExecutor.Executor,
-        _getValueForItemField = Executor._getValueForItemField;
-
-
-    Executor._getValueForItemField = function(item, field){
-        if(item instanceof Bindable){
-            return item.get(field);
-        }
-
-        return _getValueForItemField(item, field);
-    };
+define(['Query', 'js/core/Bindable'], function(Query, Bindable){
 
     var Q = Query.inherit('js.data.Query',{
-        ctor: Query,
-        /***
-         *
-         * @param items
-         * @return {Array}
-         */
-        filterItems: function (items) {
-            return Executor.filterItems(this,items);
-        },
-
-        /***
-         *
-         * @param items
-         * @return {*}
-         */
-        sortItems: function (items) {
-            return Executor.sortItems(this, items);
-        }
+        ctor: Query
     });
 
     Q.query = function(){
