61011786f3e2645e3403e2c1630b6295c15348dd
Marcus Krejpowicz <krebbl@gmail.com>
2013-03-24 14:14:00 +0100

    added hasSortExpressions

--- js/data/Query.js
+++ js/data/Query.js
@@ -1,7 +1,10 @@
 define(['Query', 'js/core/Bindable'], function(Query, Bindable){
 
     var Q = Query.inherit('js.data.Query',{
-        ctor: Query
+        ctor: Query,
+        hasSortExpressions: function(){
+            return this.query && this.query.sort && this.query.sort.length > 0;
+        }
     });
 
     Q.query = function(){
