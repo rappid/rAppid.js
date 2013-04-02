9581a744bcde59a071d50aa172107d71f3342e4e
Tony Findeisen <github@it-ony.de>
2013-04-02 18:32:25 +0200

    started write RestDataSourceTests

---
+++ test/server/rest/handler/TicketResourceHandler.js
@@ -0,0 +1,6 @@
+define(["srv/handler/rest/ResourceHandler"], function(ResourceHandler) {
+
+    return ResourceHandler.inherit('server.rest.handler.TicketResourceHandler', {
+        // TODO:
+    });
+});
