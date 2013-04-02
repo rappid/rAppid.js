fa93d1c8fe38e2c0845dacf7468110ae5fb88081
Marcus Krejpowicz <krebbl@gmail.com>
2013-04-02 08:41:27 +0200

    fixed context determination and composing

--- srv/handler/RestHandler.js
+++ srv/handler/RestHandler.js
@@ -1,13 +1,13 @@
-define(['require', 'srv/core/Handler', 'js/conf/DataSourceConfiguration', 'js/conf/ResourceConfiguration', 'srv/handler/rest/ResourceRouter', 'flow', 'js/data/DataSource', 'js/data/RestDataSource', 'js/data/Model', 'srv/lib/RestQueryParser', 'js/data/Query', 'srv/handler/rest/ResourceHandler'],
-    function (require, Handler, DataSourceConfiguration, ResourceConfiguration, ResourceRouter, flow, DataSource, RestDataSource, Model, RestQueryParser, Query, ResourceHandler) {
+define(['require', 'srv/core/Handler', 'js/conf/DataSourceConfiguration', 'js/conf/ResourceConfiguration', 'srv/handler/rest/ResourceRouter', 'flow', 'js/data/DataSource', 'js/data/RestDataSource', 'js/data/Model', 'srv/lib/RestQueryParser', 'js/data/Query', 'srv/handler/rest/ResourceHandler', 'underscore'],
+    function (require, Handler, DataSourceConfiguration, ResourceConfiguration, ResourceRouter, flow, DataSource, RestDataSource, Model, RestQueryParser, Query, ResourceHandler, _) {
 
         var RestDataProcessor = RestDataSource.RestDataProcessor.inherit('srv.handler.rest.RestDataProcessor', {
             _composeSubModel: function (model, action, options) {
                 if(model instanceof Model){
-                    return {
-                        id: model.$.id,
-                        href: model.$.href
-                    };
+                    var ret = {};
+                    ret[model.idField] = model.identifier();
+                    ret["href"] = model.$.href;
+                    return ret;
                 }
                 return this.callBase();
             },
@@ -24,7 +24,34 @@ define(['require', 'srv/core/Handler', 'js/conf/DataSourceConfiguration', 'js/co
         });
 
         var ServerRestDataSource = RestDataSource.inherit('srv.handler.rest.RestDataSource',{
-            $defaultProcessorFactory : RestDataProcessor
+            $defaultProcessorFactory : RestDataProcessor,
+            _getContext: function(factory, parent, data){
+                // here we have a combined id
+                if(factory.classof && factory.classof(Model) && data[factory.prototype.idField].indexOf("/") > -1){
+                    var ids = data[factory.prototype.idField].split("/");
+                    data[factory.prototype.idField] = ids[ids.length-1];
+                    var config = this.getConfigurationForModelClass(factory),
+                        baseConfig = config,
+                        stack = [config];
+                    for(var i = 0; i < ids.length - 1; i++){
+                        baseConfig = config.$parent;
+                        stack.unshift(baseConfig);
+                    }
+                    var parentFactory,
+                        parentModel,
+                        context;
+                    for (i = 0; i < ids.length; i++) {
+                        parentFactory = requirejs(stack[i].$.modelClassName.replace(/\./gi, "/"));
+                        context = this.getContextForChild(parentFactory, parentModel || parent);
+                        parentModel = context.createEntity(parentFactory, ids[i]);
+                        parentModel.$parent = context.$contextModel;
+                    }
+                    if(parentModel){
+                        return parentModel.$context;
+                    }
+                }
+                return this.callBase();
+            }
         });
 
         return Handler.inherit('srv.core.RestHandler', {
