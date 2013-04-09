dd529f42dd28e0cac3b1a9e6849b3261dc3b97c8
Marcus Krejpowicz <krebbl@gmail.com>
2013-04-09 17:32:29 +0200

    added data to Authentication

--- srv/core/Authentication.js
+++ srv/core/Authentication.js
@@ -1,11 +1,12 @@
 define(['js/core/Base'], function(Base) {
     return Base.inherit('srv.core.Authentication', {
-        ctor: function(authenticationRequest, user, token) {
+        ctor: function(authenticationRequest, user, data, token) {
             this.authenticationRequest = authenticationRequest;
             this.authenticationProvider = authenticationRequest.authenticationProvider;
 
             this.user = user;
-            this.token = token || user.$.id;
+            this.data = data;
+            this.token = token;
         }
 
     });
