1fb868dc819ffb358183719595bff5f4e1bd3fda
Marcus Krejpowicz <krebbl@gmail.com>
2013-05-07 23:35:22 +0200

    reimplemented TreeView ->  now with TreeNode

--- js/ui/TreeViewClass.js
+++ js/ui/TreeViewClass.js
@@ -1,85 +1,73 @@
-define(["js/ui/SelectionView", "js/core/Template", "js/core/List"], function (SelectionView, Template, List) {
+define(["js/ui/View"], function (View) {
 
-    return SelectionView.inherit({
+    var leafClass = 'tree-view-leaf',
+        rootClass = 'tree-view-root';
+
+    return View.inherit({
 
         defaults: {
-            tagName: "div",
-            items: null,
-            itemKey: 'item',
-            indexKey: 'index',
             componentClass: "tree-view",
-            keyPath: null,
-            allowDeselection: true,
-            childrenKey: "children",
-            forceSelectable: false,
-            multiSelect: true
+            node: null,
+            selectedNode: null,
+            expanded: "{node.expanded}",
+            root: "{_self()}"
         },
 
-        $defaultTemplateName: null,
+        $classAttributes: ["root", "node"],
 
-        /***
-         * Creates a component based on the template for a given item
-         * @param {Object} item
-         * @param {Number} index
-         * @return {js.core.Component} component
-         * @private
-         */
-        _createComponentForItem: function (item, index) {
-            var children = this.get(item, this.$.childrenKey);
-            var component;
-            if (children instanceof List || children instanceof Array) {
+        _initialize: function () {
+            this.callBase();
+            this.bind('root', 'change:selectedNode', this._onSelectedNodeChange, this);
+        },
 
-                component = this.getTemplate("node-container").createComponents(attr)[0];
+        _onSelectedNodeChange: function (e) {
 
-                var attr = {};
-                attr[this._getItemKey()] = item;
-                attr[this._getIndexKey()] = index;
-                var node = this.getTemplate("node").createComponents(attr)[0];
-                if (node.$classAttributes) {
-                    node.$classAttributes.push(this.$.itemKey, this.$.indexKey);
+            var node = e.$;
+
+            this.set('selected', node === this.$.node);
+        },
+
+        _renderNode: function (node) {
+            if (node) {
+                if (node.$.isLeaf) {
+                    this.addClass(leafClass);
+                } else {
+                    this.removeClass(leafClass);
                 }
-
-                node.bind("on:click", function (e) {
-                    e.stopPropagation();
-
-                    node.$parent.set('selected', !node.$parent.$.selected);
-                });
-
-                var listAttr = {
-                    itemKey: this._getItemKey(),
-                    indexKey: this._getIndexKey(),
-                    items: children,
-                    _root: this.$._root || this
-                };
-
-                var treeView = this.createComponent(this.factory, listAttr);
-
-                treeView.addChild(this.$templates["node"]);
-                treeView.addChild(this.$templates["item"]);
-
-                if (treeView.$classAttributes) {
-                    treeView.$classAttributes.push(this.$.itemKey, this.$.indexKey);
-
+                if (node.$.isRoot) {
+                    this.addClass(rootClass);
+                } else {
+                    this.removeClass(rootClass);
                 }
-
-                component.addChild(node);
-                component.addChild(treeView);
-
             } else {
-                component = this.callBase();
-                component.set('selectable', true);
-                component.bind("change:selected", this._onItemSelected, this.$._root || this);
+                this.removeClass(leafClass);
+                this.removeClass(rootClass);
             }
-            return component;
+
         },
 
-        _onItemSelected: function (e) {
-            if (e.$) {
-                if (this.$selectedItemComponent) {
-                    this.$selectedItemComponent.set('selected', false);
-                }
-                this.$selectedItemComponent = e.target;
+        _renderExpanded: function (expanded) {
+            if (expanded) {
+                this.addClass("expanded");
+            } else {
+                this.removeClass("expanded");
             }
+        },
+
+        _handleClick: function () {
+            if (this.$.node) {
+                if (!this.$.node.$.isLeaf) {
+                    this.$.node.toggle();
+                }
+                if (this.$.node.$.expanded || this.$.node.$.isLeaf) {
+                    var root = this.$.root;
+                    root.set('selectedNode', this.$.node);
+                }
+            }
+        },
+
+        _self: function () {
+            return this;
         }
     });
 });
