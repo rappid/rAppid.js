5c3f47e86ac5783f814a53e6a22ac1a2aba778f6
Tony Findeisen <github@it-ony.de>
2013-05-07 16:04:46 +0200

    remove idField from compose Payload in Model if action is create

--- js/data/Model.js
+++ js/data/Model.js
@@ -154,6 +154,16 @@ define(["js/data/Entity", "js/core/List", "flow", "underscore"], function (Entit
 
         },
 
+        compose: function (action, options) {
+            var ret = this.callBase();
+
+            if (action === "create" && ret.hasOwnProperty(this.idField)) {
+                delete  ret[this.idField];
+            }
+
+            return ret;
+        },
+
         /**
          * @param options
          * @param options.fetchSubModels
