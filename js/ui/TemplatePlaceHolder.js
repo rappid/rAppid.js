609bd547a7693cc9771fd1b10792e1ce14c076b5
Marcus Krejpowicz <krebbl@gmail.com>
2013-06-19 17:25:40 +0200

    allowed mutliple children for TemplatePlaceHolder

--- js/ui/TemplatePlaceHolder.js
+++ js/ui/TemplatePlaceHolder.js
@@ -15,32 +15,39 @@ define(["js/ui/View"], function (View) {
 
         $classAttributes: ["item", "templateName"],
 
-        _renderTemplateName: function(templateName){
+        _renderTemplateName: function (templateName) {
             if (templateName) {
                 if (!this.$template) {
                     this.$template = this.getTemplate(templateName);
                 }
 
-                if(!this.$templateInstance){
-                    this.$templateInstance = this.$template.createInstance({
+                if (!this.$templateChildren) {
+                    this.$templateChildren = this.$template.createComponents({
                         item: this.$.item
                     }, this.$parentScope, this.$rootScope);
 
-                    if(this.$templateInstance.$classAttributes){
-                        this.$templateInstance.$classAttributes.push("item");
+                    var child;
+                    for (var i = 0; i < this.$templateChildren.length; i++) {
+                        child = this.$templateChildren[i];
+                        if (child.$classAttributes) {
+                            child.$classAttributes.push("item");
+                        }
+
+                        this.addChild(child);
                     }
 
-                    this.addChild(this.$templateInstance);
                 }
             } else {
                 this.$template = null;
                 // TODO: remove child
             }
         },
-        _commitChangedAttributes: function(attributes){
+        _commitChangedAttributes: function (attributes) {
             this.callBase();
-            if(this.$templateInstance && attributes.hasOwnProperty("item")){
-                this.$templateInstance.set("item",attributes["item"]);
+            if (this.$templateChildren && attributes.hasOwnProperty("item")) {
+                for (var i = 0; i < this.$templateChildren.length; i++) {
+                    this.$templateChildren[i].set("item", attributes["item"]);
+                }
             }
         }
     });
