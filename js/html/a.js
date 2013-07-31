ce20ce3123c312f7f587bd790d0ceb4a44b106f8
Tony Findeisen <github@it-ony.de>
2013-07-31 11:28:02 +0200

    added support for pushstate

--- js/html/a.js
+++ js/html/a.js
@@ -5,21 +5,47 @@ define(['js/html/HtmlElement'], function (HtmlElement) {
         hashBankUrl = /^#?(.*)$/;
 
     return HtmlElement.inherit("js.html.a", {
+
         defaults: {
             tagName: 'a',
             target: null,
             href: null
         },
+
+        _initializationComplete: function() {
+            this.callBase();
+
+            var history = this.$stage.$history;
+
+            if (history.$.useState) {
+                this.bind("on:click", function (e) {
+                    e.preventDefault();
+                    history.navigate(this.plainHref());
+                }, this);
+            }
+
+        },
+
+        plainHref: function() {
+            var href = this.$.href || "";
+            return href.replace(stripHashSlash, '');
+        },
+
         _renderHref: function (href) {
             href = href || "javascript:void(0);";
 
             if (!(this.$.target === "external" || externalLink.test(href) || this.$.target === "_blank")) {
 
-                href = "#/" + href.replace(stripHashSlash, '');
+                var plainHref = this.plainHref();
+                href = "#/" + plainHref;
 
                 if (!this.runsInBrowser()) {
                     // node rendering -> hash bang url
                     href = href.replace(hashBankUrl, "#!$1");
+                } else if (this.$stage.$history.$.useState) {
+                    // we use push state methods -> show plain href
+                    // but register a click handler which will perform a push state
+                    href = plainHref;
                 }
             }
 
