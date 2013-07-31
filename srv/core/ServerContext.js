289e4b45e459ef856936d283db325e79544e935f
Marcus Krejpowicz <krebbl@gmail.com>
2013-07-31 23:48:52 +0200

    fixed creation of Bus in ServerContext

--- srv/core/ServerContext.js
+++ srv/core/ServerContext.js
@@ -6,7 +6,7 @@ define(['js/core/Base', 'js/core/Bus'], function(Base, Bus) {
             this.$requirejsContext = requireJsContext;
             this.$applicationContext = applicationContext;
 
-            this.$bus = new Bus();
+            this.$bus = new Bus(this);
         }
     });
 
