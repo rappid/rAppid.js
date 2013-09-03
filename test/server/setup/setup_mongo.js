36d3173295853a95e1a25a5491711a9e9029a68b
Marcus Krejpowicz <krebbl@gmail.com>
2013-09-03 13:44:37 +0200

    added index for restapi tests

--- test/server/setup/setup_mongo.js
+++ test/server/setup/setup_mongo.js
@@ -1,4 +1,6 @@
 db.createCollection("projects");
+db.projects.ensureIndex({name: 1}, {unique: true});
+
 
 db.createCollection("tickets");
 db.tickets.ensureIndex({key: 1}, {unique: true});
@@ -6,5 +8,9 @@ db.tickets.ensureIndex({key: 1}, {unique: true});
 db.createCollection("comments");
 db.createCollection("users");
 db.users.ensureIndex({email: 1}, {unique: true});
+db.createCollection("issueTypes");
+db.issueTypes.ensureIndex({key: 1}, {unique: true});
+
+
 db.createCollection("identities");
 db.createCollection("authentications");
