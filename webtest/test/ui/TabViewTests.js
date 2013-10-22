f0ecbcf599caaff075f46c606ef6782bf935ee03
Marcus Krejpowicz <krebbl@gmail.com>
2013-10-22 10:39:46 +0200

    added some more tests for TabView

--- webtest/test/ui/TabViewTests.js
+++ webtest/test/ui/TabViewTests.js
@@ -17,7 +17,7 @@ describe("#TabView", function () {
 
         });
 
-        it('should switch tab when clicking on another tab-item', function(){
+        it('should switch tab when clicking on another tab-item', function () {
 
             $("#t1 ul li:not(.active)").eq(0).click();
             $expect('#t1 ul li.active').to.exist()
@@ -29,11 +29,41 @@ describe("#TabView", function () {
 
         });
 
-        it('should not switch tab when click on active again', function(){
+        it('should not switch tab when click on active again', function () {
 
             var text = $('#t1 .tab-content').eq(0).text();
             $("#t1 ul li.active").eq(0).click();
             $expect('#t1 .tab-content').to.contain(text);
+
+        });
+
+    });
+
+    describe('selected tab attribute', function () {
+
+        var tabView;
+        beforeEach(function () {
+            tabView = window.application.$.t1;
+        });
+
+        it('should contain the selected tab', function () {
+
+            $expect('#t1 .tab-content').to.contain(tabView.$.selectedTab.$.title);
+
+        });
+
+        it('should change active tab and tab-content when setting other selected tab', function () {
+
+            var currentTab = tabView.$.selectedTab;
+            var thirdTab = window.application.$["t1-3"];
+            tabView.set('selectedTab', thirdTab);
+
+            $expect('#t1 .tab-content').to.contain(thirdTab.$.title);
+
+            $expect('#t1 ul li.active').to.exist().and.to.contain(thirdTab.$.title);
+
+
+            tabView.set('selectedTab', currentTab);
 
         });
 
