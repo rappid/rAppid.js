a6563c438e99f15f477c53c5c1fa9e925f54a958
Marcus Krejpowicz <krebbl@gmail.com>
2013-10-24 15:50:33 +0200

    AuthenticationError now inherits from HttpError

--- srv/auth/AuthenticationError.js
+++ srv/auth/AuthenticationError.js
@@ -1,11 +1,10 @@
-define([], function () {
+define(["srv/core/HttpError"], function (HttpError) {
 
-    var AuthenticationError = Error.inherit('srv.authentication.AuthenticationError', {
-        ctor: function (message, errorCode) {
+    var AuthenticationError = HttpError.inherit('srv.authentication.AuthenticationError', {
+        ctor: function (message, errorCode, statusCode) {
 
-            Error.prototype.constructor.call(this, message, errorCode);
+            this.callBase(message, statusCode, errorCode);
 
-            this.message = message;
             this.errorCode = errorCode;
         },
 
@@ -22,11 +21,11 @@ define([], function () {
 
     });
 
-    AuthenticationError.WRONG_USERNAME_OR_PASSWORD = new AuthenticationError("Wrong username or password", "wrong_username_or_password");
-    AuthenticationError.TOO_MANY_WRONG_ATTEMPTS = new AuthenticationError("Too many wrong login attempts", "too_many_wrong_login_attempts");
-    AuthenticationError.AUTHENTICATION_EXPIRED = new AuthenticationError("Authentication expired", "authentication_expired");
-    AuthenticationError.NO_PROVIDER_FOUND = new AuthenticationError("No authentication provider found for request", "no_provider_found");
-    AuthenticationError.NO_IDENTITY_FOUND = new AuthenticationError("No Identity found!", "no_identity_found");
+    AuthenticationError.WRONG_USERNAME_OR_PASSWORD = new AuthenticationError("Wrong username or password", "wrong_username_or_password", 401);
+    AuthenticationError.TOO_MANY_WRONG_ATTEMPTS = new AuthenticationError("Too many wrong login attempts", "too_many_wrong_login_attempts", 401);
+    AuthenticationError.AUTHENTICATION_EXPIRED = new AuthenticationError("Authentication expired", "authentication_expired", 401);
+    AuthenticationError.NO_PROVIDER_FOUND = new AuthenticationError("No authentication provider found for request", "no_provider_found", 401);
+    AuthenticationError.NO_IDENTITY_FOUND = new AuthenticationError("No Identity found!", "no_identity_found", 401);
 
     return AuthenticationError;
 });
