12f43d77bcc6066b4016f354590cb827635f8502
Marcus Krejpowicz <krebbl@gmail.com>
2013-10-28 14:20:49 +0100

    implemented NotificationManager

---
+++ js/core/NotificationManager.js
@@ -0,0 +1,58 @@
+define(["js/core/Component", "js/html/HtmlElement"], function (Component, HtmlElement) {
+
+    return Component.inherit('js.core.Component', {
+
+        defaults: {
+            duration: 3,
+            containerClass: "notifications"
+        },
+
+        ctor: function () {
+            this.$notifications = [];
+
+            this.callBase();
+        },
+
+        showNotification: function (templateName, attributes, options) {
+            if (!this.$container) {
+                this.$container = this.createComponent(HtmlElement, {"class": this.$.containerClass, tagName: "div"});
+                this.$stage.addChild(this.$container);
+            }
+
+            if (!this.$templates[templateName]) {
+                console.warn("Couldn't find template " + templateName);
+                return null;
+            }
+
+            var duration = this.$.duration || options;
+
+            var notification = this.$templates[templateName].createInstance(attributes);
+
+            notification.bind('remove:dom', function () {
+                notification.destroy();
+            });
+
+            this.$container.addChild(notification);
+
+            var self = this;
+            notification.close = function () {
+                self.closeNotification(this);
+            };
+            this.$notifications.push(notification);
+
+            setTimeout(function () {
+                self.closeNotification(notification);
+            }, duration * 1000);
+
+
+            return notification;
+        },
+
+        closeNotification: function (notification) {
+            this.$notifications.splice(this.$notifications.indexOf(notification), 1);
+            this.$container.removeChild(notification);
+        }
+
+
+    });
+});
