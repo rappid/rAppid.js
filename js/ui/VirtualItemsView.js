1d7779e5c22b7ac788a01a7cad4cbea65a6147ef
Marcus Krejpowicz <krebbl@gmail.com>
2014-05-23 13:14:22 +0200

    fixed event for item click ...

--- js/ui/VirtualItemsView.js
+++ js/ui/VirtualItemsView.js
@@ -142,16 +142,16 @@ define(['js/ui/View', 'js/core/Bindable', 'js/core/List', 'js/data/Collection', 
                 var self = this;
 
                 this.bindDomEvent('scroll', scroll);
-                this.bindDomEvent("mousemove", function (e) {
-                    self._mouseMove(e);
-                });
-                this.bindDomEvent("mouseout", function (e) {
-                    if (self.$.hoverItem) {
-                        self.trigger("on:itemMouseOut", null, self.$.hoverItem);
-                    }
-
-                    self.set('hoverItem', null);
-                });
+//                this.bindDomEvent("mousemove", function (e) {
+//                    self._mouseMove(e);
+//                });
+//                this.bindDomEvent("mouseout", function (e) {
+//                    if (self.$.hoverItem) {
+//                        self.trigger("on:itemMouseOut", null, self.$.hoverItem);
+//                    }
+//
+//                    self.set('hoverItem', null);
+//                });
 
                 function scroll(e) {
                     self.set({
@@ -403,7 +403,7 @@ define(['js/ui/View', 'js/core/Bindable', 'js/core/List', 'js/data/Collection', 
                 }
                 var renderer = this._createRenderer();
                 renderer.bind('on:dblclick', this._onRendererDblClick, this);
-                renderer.bind('on:pointer', this._onRendererClick, this);
+                renderer.bind('on:click', this._onRendererClick, this);
                 return renderer;
             },
 
