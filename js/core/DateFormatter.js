5bc3e8b6b264069f1d1569d770c48640265e1a32
Tony Findeisen <github@it-ony.de>
2014-06-02 17:45:59 +0200

    refactored I18n to have DateFormatting with js/core/DateFormatter

---
+++ js/core/DateFormatter.js
@@ -0,0 +1,32 @@
+define(["js/core/Component", "moment"], function (Component, moment) {
+    return Component.inherit("js.core.I18n", {
+        defaults: {
+            /**
+             * The default locale
+             *
+             * @type String
+             */
+            locale: null
+        },
+
+
+        /***
+         * Formats a date value to the given format
+         *
+         * @param {Date|Number} value - the value to format
+         * @param {String} [value] -  the format
+         */
+        f: function (value, format) {
+
+            if (value instanceof Date) {
+                format = format || "LLL";
+                return moment(value).format(format);
+            } else if (value instanceof moment) {
+                format = format || "LLL";
+                return value.format(format);
+            }
+
+            return value;
+        }.on("localeChanged")
+    });
+});
