904a6b9158b5715ccc1d032bea1b38142e03b54c
Marcus Krejpowicz <krebbl@gmail.com>
2014-06-06 11:10:47 +0200

    * Field now gets active class when first input element is focused

--- js/ui/FieldClass.js
+++ js/ui/FieldClass.js
@@ -11,6 +11,7 @@ define(["js/ui/View", "js/html/Input", "js/html/Select", "js/html/TextArea", 'js
             enabled: true,
             error: null,
             required: false,
+            focused: false,
             requiredClass: "required"
         },
 
@@ -45,15 +46,40 @@ define(["js/ui/View", "js/html/Input", "js/html/Select", "js/html/TextArea", 'js
             var firstChild;
 
             for (var j = 0; j < children.length; j++) {
-                firstChild = this.getFirstChild(children[j]);
+                firstChild = this.findFirstInput(children[j]);
                 if (firstChild) {
+                    this.$firstInput = firstChild;
                     if (!firstChild.$.id) {
                         firstChild.set('id', this.$.inputId)
                     }
-                    return;
+                    break;
                 }
             }
 
+            if (this.$firstInput && !/radio|checkobox/i.test(this.$firstInput.$.type)) {
+                // only for input that are not radio or checkobox and select and textareas
+                var self = this;
+                this.$firstInput.bind('on:focus', function () {
+                    self.set('selected', true);
+                });
+                this.$firstInput.bind('on:blur', function () {
+                    self.set('selected', false);
+                });
+
+            }
+
+        },
+
+        focusFirstInput: function () {
+            if (this.$firstInput) {
+                this.$firstInput.focus();
+            }
+        },
+
+        blurFirstInput: function () {
+            if (this.$firstInput) {
+                this.$firstInput.blur();
+            }
         },
 
         _renderRequired: function (required) {
@@ -64,7 +90,7 @@ define(["js/ui/View", "js/html/Input", "js/html/Select", "js/html/TextArea", 'js
             }
         },
 
-        getFirstChild: function (child) {
+        findFirstInput: function (child) {
             if (child instanceof Input || child instanceof Select || child instanceof TextArea) {
                 return child;
             }
@@ -72,7 +98,7 @@ define(["js/ui/View", "js/html/Input", "js/html/Select", "js/html/TextArea", 'js
             if (child && child.$children) {
                 var children = child.getViewChildren();
                 for (var i = 0; i < children.length; i++) {
-                    var c = this.getFirstChild(children[i]);
+                    var c = this.findFirstInput(children[i]);
                     if (c) {
                         return c;
                     }
