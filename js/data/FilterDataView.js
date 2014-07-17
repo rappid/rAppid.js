1ebbfa0a97a0a2984bfeb2608a5104d167f76b3c
Tony Findeisen <github@it-ony.de>
2014-07-17 13:41:47 +0200

    added defaults to FilterDataView

--- js/data/FilterDataView.js
+++ js/data/FilterDataView.js
@@ -8,22 +8,31 @@ define(["js/data/DataView", "underscore"], function (DataView, _) {
 
     return DataView.inherit("js.data.FilterDataView", {
 
+        defaults: {
+            filterFnc: null
+        },
+
         initialize: function(){
             this.callBase();
             this.bind('change', this._onFilterChanged, this);
         },
+
         _onFilterChanged: function(){
-            this._innerReset(this.$.baseList.$items);
+            var baseList = this.$.baseList;
+            this._innerReset(baseList ? baseList.$items : null);
         },
+
         _onItemAdded: function (e) {
             var ret = this._filterItem(e.$.item, e.$.index);
             if (ret === true) {
                 this.$.list.add(e.$.item, e.$.index);
             }
         },
+
         _onItemRemoved: function (e) {
             this.$.list.remove(e.$.item, e.$.index);
         },
+
         _onItemChanged: function (e) {
             var keep = this._filterItem(e.$.item, e.$.index);
             var included = _.include(this.$.list.$items, e.$.item);
@@ -33,7 +42,10 @@ define(["js/data/DataView", "underscore"], function (DataView, _) {
                 this.$.list.add(e.$.item, e.$.index);
             }
         },
+
         _innerReset: function(items){
+            items = items || [];
+
             var filtered = [], item;
             for (var i = 0; i < items.length; i++) {
                 item = items[i];
@@ -43,9 +55,12 @@ define(["js/data/DataView", "underscore"], function (DataView, _) {
             }
             this.$.list.reset(filtered);
         },
+
         _filterItem: function (item, index) {
-            if(this.$.filterFnc){
-                return this.$.filterFnc.call(this, item, index, this);
+            var filterFnc = this.$.filterFnc;
+
+            if(filterFnc){
+                return filterFnc.call(this, item, index, this);
             }
             return true;
         }
