ba009b5b212badf15b68dc5d3540541367738bdf
Marcus Krejpowicz <krebbl@gmail.com>
2014-11-12 17:43:58 +0100

    fixed EmailValidator

--- js/data/validator/EmailValidator.js
+++ js/data/validator/EmailValidator.js
@@ -3,7 +3,7 @@ define(['js/data/validator/RegExValidator'], function (RegExValidator) {
     return RegExValidator.inherit('js.data.validator.EmailValidator', {
         defaults: {
             // TODO: add umlauts
-            regEx: /^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,6})+$/,
+            regEx: /^([a-zA-Z0-9_\.\-])+@(([a-zA-Z0-9\-.])+\.)+([a-zA-Z0-9]{2,6})$/,
             errorCode: 'emailError'
         }
     });
